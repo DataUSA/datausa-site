e74620bd38b20d9d1b0a280c4d6477888f91560e
Dave Landry <landry.dave@gmail.com>
2015-10-13 09:47:59 -0400

    wrote gzipped and datasplit functions

--- datausa/utils.py
+++ datausa/utils.py
@@ -1,10 +1,18 @@
-import requests
+import functools, gzip, requests
+from flask import after_this_request, request
 from config import API
 from datausa import cache, app
+from cStringIO import StringIO as IO
 
 def datafold(data):
     """List[dict]: combines the headers and data from an API call """
     return [dict(zip(data["headers"], d)) for d in data["data"]]
+
+def datasplit(data):
+    data = [v for (k,v) in data.items()]
+    headers = [k for (k,v) in data[0].items()]
+    data = [[v for (k, v) in d.items()] for d in data]
+    return {"data": data, "headers": headers}
 
 def fetch(attr_id, attr_type):
     """dict: Returns an attribute dict container information like 'name' and 'color' """
@@ -32,3 +40,38 @@ def build_attr_cache():
     return results
 
 attr_cache = build_attr_cache()
+
+''' Decorator for returning gzipped content via http://flask.pocoo.org/snippets/122/ '''
+def gzipped(f):
+    @functools.wraps(f)
+    def view_func(*args, **kwargs):
+
+        @after_this_request
+        def zipper(response):
+            accept_encoding = request.headers.get('Accept-Encoding', '')
+
+            if 'gzip' not in accept_encoding.lower():
+                return response
+
+            response.direct_passthrough = False
+
+            if (response.status_code < 200 or
+                response.status_code >= 300 or
+                'Content-Encoding' in response.headers):
+                return response
+            gzip_buffer = IO()
+            gzip_file = gzip.GzipFile(mode='wb',
+                                      fileobj=gzip_buffer)
+            gzip_file.write(response.data)
+            gzip_file.close()
+
+            response.data = gzip_buffer.getvalue()
+            response.headers['Content-Encoding'] = 'gzip'
+            response.headers['Vary'] = 'Accept-Encoding'
+            response.headers['Content-Length'] = len(response.data)
+
+            return response
+
+        return f(*args, **kwargs)
+
+    return view_func
