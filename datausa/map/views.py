c46f6e7a6dd4571c1cc143c910f6a275daf04a7e
Melissa Teng <tengmelis@gmail.com>
2015-10-23 18:40:57 -0400

    desktop home structure start

--- datausa/map/views.py
+++ datausa/map/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, g, render_template
 
 mod = Blueprint("map", __name__, url_prefix="/map")
 
 @mod.route("/")
 def map():
+    g.page_type = "map"
     return render_template("map/index.html")
--- datausa/story/views.py
+++ datausa/story/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, g, render_template
 
 mod = Blueprint("story", __name__, url_prefix="/story")
 
 @mod.route("/")
 def index():
-    return render_template("story/index.html")
+	g.page_type = "story"
+	return render_template("story/index.html")
