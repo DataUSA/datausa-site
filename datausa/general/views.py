b56d8cf59a8950434164361dad1f9caf0e47a8df
Melissa Teng <tengmelis@gmail.com>
2015-11-20 13:59:45 -0500

    click out of menu to close & new home images

--- datausa/general/views.py
+++ datausa/general/views.py
@@ -20,5 +20,6 @@ def before_request():
 @mod.route("/")
 def home():
     g.page_type = "home"
-    bg_imgs = randint(1,6)
+    bg_imgs = randint(1,4)
     return render_template("general/home.html", bg_imgs = bg_imgs)
+ 
