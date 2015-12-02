be1037c869b9ed304723731219d902d9703d1156
Dave Landry <landry.dave@gmail.com>
2015-12-02 18:26:01 -0500

    fixed axes with defined tick values

--- datausa/assets/js/viz/configs/defaults.js
+++ datausa/assets/js/viz/configs/defaults.js
@@ -43,6 +43,10 @@ viz.defaults = function(build) {
       build.config[axis].label = label;
     }
 
+    if (build.config[axis] && build.config[axis].ticks && build.config[axis].ticks.value) {
+      build.config[axis].ticks.value = JSON.parse(build.config[axis].ticks.value);
+    }
+
     var range = proportions.indexOf(key) >= 0 && key !== "pct_total" ? [0, 1] : false;
 
     var key = axis.length === 1 ? "pri" : "sec",
