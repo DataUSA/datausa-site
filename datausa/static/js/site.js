87183bc8e5c56f0595b0b6bfe8ef3f3008c2c14d
Dave Landry <landry.dave@gmail.com>
2015-12-08 12:39:01 -0500

    show data/nav button ga events only fire once

--- datausa/static/js/site.js
+++ datausa/static/js/site.js
@@ -328,7 +328,7 @@ window.onload = function() {
     else {
 
     }
-    
+
     // "ESC" button
     if (d3.event.keyCode === 27) {
       // close all search results
@@ -343,14 +343,14 @@ window.onload = function() {
   // Key events while the search input is active
   var searchInterval, keywait = 300;
   d3.selectAll(".search-input").on("keyup.search-input", function(){
-    
+
     // "ESC" button
     if (d3.event.keyCode === 27) {
       d3.select(".search-box").classed("open", false);
       d3.select("#search-simple-nav").classed("open", false);
       d3.select(".search-box input").node().blur();
     }
-    
+
     // Enter button
     if (d3.event.keyCode === 13) {
       var search_txt = d3.select(this).property("value");
@@ -358,7 +358,7 @@ window.onload = function() {
     }
 
     var q = this.value.toLowerCase();
-    
+
     if(this.id == "nav-search-input"){
       if(q === "") {
         // d3.select("#search-simple-nav").style("display", "none")
@@ -371,7 +371,7 @@ window.onload = function() {
         console.log(d3.select("#search-simple-nav").node())
       }
     }
-    
+
     if (q !== search.term) {
       clearInterval(searchInterval);
       search.term = q;
@@ -389,13 +389,13 @@ window.onload = function() {
     }
 
   });
-  
+
   d3.selectAll(".search-input, .search-results").on("keyup.search-results", function(){
-    
+
     // Up/Down Arrows
     if (d3.event.keyCode === 40 || d3.event.keyCode === 38) {
       var up = d3.event.keyCode === 38;
-      
+
       // get current active element
       var curr_el = d3.select(this).select("a.search-item:focus").node();
       if(curr_el){
@@ -412,14 +412,14 @@ window.onload = function() {
       else if(!up){
         var next_el = document.querySelectorAll(".search-item")[0];
       }
-      
+
       if(next_el) next_el.focus();
-      
-      
+
+
       d3.event.preventDefault();
       return false;
     }
-    
+
     // Enter
     if (d3.event.keyCode === 13) {
       var curr_el = d3.select(this).select("a.search-item:focus").node();
@@ -428,9 +428,9 @@ window.onload = function() {
         window.location = "/search/?q="+search_txt;
       }
     }
-    
+
   });
-  
+
   d3.selectAll(".search-results").on("keydown.search-results", function(){
     // Up/Down Arrows
     if (d3.event.keyCode === 40 || d3.event.keyCode === 38) {
@@ -438,29 +438,39 @@ window.onload = function() {
       return false;
     }
   });
-  
-  
+
+
   d3.selectAll("[data-ga]").on("click.ga", function(){
-  
+
     var _this = d3.select(this);
     var action = _this.attr("data-ga") || "click";
     var category = _this.attr("data-ga-cat") || "general";
     var label = _this.attr("data-ga-label") || "n/a";
-    var target = _this.attr("data-ga-target");
-  
-    if(action == "show data"){
-      target = d3.select(this.parentNode.parentNode).select(target).node();
-    }
-    
-    console.log("GA, action: ", action, "category: ", category, "label: ", label)
+    var target = _this.attr("data-ga-target") || false;
+    var send = true;
 
-    ga('send', {
-      hitType: 'event',
-      eventCategory: category,
-      eventAction: action,
-      eventLabel: label
-    });
-  
+    if(target){
+      var parent = this.parentNode;
+      if (action == "show data") {
+        parent = parent.parentNode;
+      }
+      target = d3.select(parent).select(target);
+      send = target.classed("visible") || target.classed("open");
+    }
+
+    if (send) {
+
+      console.log("GA, action: ", action, "category: ", category, "label: ", label)
+
+      ga('send', {
+        hitType: 'event',
+        eventCategory: category,
+        eventAction: action,
+        eventLabel: label
+      });
+
+    }
+
   })
 
 }
