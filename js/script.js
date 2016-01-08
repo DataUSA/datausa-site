a3c1d2d429095adcf0006cdbc2c49ad4415f0abe
Melissa Teng <tengmelis@gmail.com>
2016-01-07 23:57:05 -0500

    fixed hero img background-sizing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(function() {
+  heroImg();
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -11,6 +13,20 @@ $(function() {
       }
     }
   });
+
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), 500);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
 });
 
 var TxtRotate = function(el, toRotate, period) {
@@ -54,18 +70,18 @@ TxtRotate.prototype.tick = function() {
   }, delta);
 };
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), 500);
-    }
+function heroImg() {
+  var heroW = $('#hero-img').width(),
+      heroH = $('#hero-img').height(),
+      heroR = heroW/heroH;
+
+  if (heroR > 4587 / 3677) {
+    $('#hero-img').css("background-size", "cover").css("background-position", "0 100%");
+  } else {
+    $('#hero-img').css("background-size", "contain").css("background-position", "0 0");
   }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
+}
+
+$( window ).resize(function() {
+  heroImg();
+});
