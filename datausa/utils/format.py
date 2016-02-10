13cebe2d2659198f62b9d2584a44733d93be4674
Dave Landry <landry.dave@gmail.com>
2016-02-10 17:56:51 -0500

    CES NAICS splash numbers (closes #178)

--- datausa/utils/format.py
+++ datausa/utils/format.py
@@ -37,7 +37,7 @@ def num_format(number, key=None, labels=True, condense=True, suffix=True):
     # Converts the number to a float.
     n = float(number)
 
-    if key and "emp_thousands" in key:
+    if key and ("emp_thousands" in key or "employees_thousands" in key):
         n = n * 1000
     elif key == "value_millions":
         n = n * 1000000
