55e50ca1adf08666438a048cf893414a56399a0f
Dave Landry <landry.dave@gmail.com>
2016-03-16 11:34:55 -0400

    fixes small number formatting (closes #288)

--- datausa/assets/js/viz/data/format.js
+++ datausa/assets/js/viz/data/format.js
@@ -31,7 +31,7 @@ viz.format = {
 
       if (proportions.indexOf(key) >= 0) number = number * 100;
 
-      if (number < 999999.99) {
+      if (number < 999999.99 && number >= 0.1) {
         var prec = key in affixes ? "2" : "1";
         number = d3.format(",." + prec + "f")(number);
         number = prec === "2" ? number.replace(".00", "") : number.replace(".0", "");
