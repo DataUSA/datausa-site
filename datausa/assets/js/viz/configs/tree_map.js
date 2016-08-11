426f59e5d5cdac2823abd567f0ba80cd85b33c86
Dave Landry <landry.dave@gmail.com>
2016-08-11 12:37:16 -0400

    adds total values above tree maps (closes #146)

--- datausa/assets/js/viz/configs/tree_map.js
+++ datausa/assets/js/viz/configs/tree_map.js
@@ -6,6 +6,20 @@ viz.tree_map = function(build) {
     },
     "legend": {
       "filters": true
+    },
+    "title": {
+      "total": {
+        "font": {
+          "color": "#444",
+          "family": "Palanquin",
+          "size": 14,
+          "transform": "uppercase",
+          "weight": 700
+        },
+        "value": {
+          "prefix": dictionary[build.config.size] + ": "
+        }
+      }
     }
   };
 }
