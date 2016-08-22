6dbc8b6b803af96163ce02c43169e3282d0f3723
Dave Landry <landry.dave@gmail.com>
2016-08-22 17:48:52 -0400

    removes console statement

--- datausa/assets/js/viz/viz.js
+++ datausa/assets/js/viz/viz.js
@@ -123,8 +123,6 @@ viz.finish = function(build) {
   var years = d3plus.util.uniques(build.viz.data(), function(d) { return d.year; }),
       axis = build.config.x ? build.config.x.value : null;
 
-  console.log(years);
-
   if (years.length > 1 && axis !== build.viz.time()) {
     if (!build.config.ui) build.config.ui = [];
     var focus = d3.max(build.viz.data(), function(d) { return d.year; });
