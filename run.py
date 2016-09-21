264eaa1d065707e32f26812e21abb596f575aabd
Dave Landry <landry.dave@gmail.com>
2016-09-21 12:02:45 -0400

    updates run.py to correctly reload jinja templates

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
+import os
 from datausa import app
-from flask.ext.script import Manager
+# from flask.ext.script import Manager
 
 if __name__ == '__main__':
     app.debug = True
-    manager = Manager(app)
-    manager.run()
+    extra_dirs = ["datausa/html"]
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in os.walk(extra_dir):
+            for filename in files:
+                filename = os.path.join(dirname, filename)
+                if os.path.isfile(filename):
+                    extra_files.append(filename)
+    app.run(extra_files=extra_files)
+    # manager = Manager(app)
+    # manager.run(extra_files=extra_files)
