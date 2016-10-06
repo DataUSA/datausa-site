a977df9f639519ef407f0d75c42886e006355ec0
Dave Landry <landry.dave@gmail.com>
2016-10-06 10:31:44 -0400

    memoizes profile section loading

--- datausa/profile/profile.py
+++ datausa/profile/profile.py
@@ -41,6 +41,7 @@ class Profile(BaseObject):
         self.attr_type = attr_type
         self.variables = self.load_vars(required_namespaces)
         self.splash = Section(self.load_yaml(self.open_file("splash")), self)
+        self.section_cache = False
 
     def children(self, **kwargs):
 
@@ -224,7 +225,7 @@ class Profile(BaseObject):
         config = json.dumps(config)
 
         # regex to find all keys matching {{*}}
-        def conditionalParse(s, o="{", c="}"):
+        def conditionalParse(s, o="{"):
             if o == "{":
                 ro = "\{"
                 rc = "\}"
@@ -267,9 +268,7 @@ class Profile(BaseObject):
                 else:
                     if o == "{":
                         if "[[" in text:
-                            # raise Exception(text, k, s)
-                            text = conditionalParse(text, "[", "]")
-                            # raise Exception(k, text, s)
+                            text = conditionalParse(text, "[")
                         s = s.replace("{{{{{0}}}}}".format(k), text)
                     else:
                         s = s.replace("[[{0}]]".format(k), text)
@@ -618,9 +617,11 @@ class Profile(BaseObject):
 
     def sections(self):
         """list[Section]: Loads YAML configuration files and converts them to Section classes. """
+        if not self.section_cache:
+            # pass each file to the Section class and return the final array
+            self.section_cache = [Section(self.load_yaml(self.open_file(f)), self, f) for f in self.splash.sections]
 
-        # pass each file to the Section class and return the final array
-        return [Section(self.load_yaml(self.open_file(f)), self, f) for f in self.splash.sections]
+        return self.section_cache
 
     def section_by_topic(self, section_name, slugs):
         '''Section: Creates a custom Section object with the desired topics by slug'''
--- datausa/profile/views.py
+++ datausa/profile/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import abort, Blueprint, g, jsonify, render_template, request, redirect, url_for
 from config import CROSSWALKS, PROFILES
+from datausa import cache
 from datausa.profile.profile import Profile
 from datausa.utils.data import attr_cache, acs_crosswalk, fetch
 from datausa.utils.manip import stat
@@ -14,14 +15,19 @@ mod = Blueprint("profile", __name__, url_prefix="/profile")
 def before_request():
     g.page_type = "profile"
 
-# create a route and function for the education profile that accepts a CIP id
 @mod.route("/")
 def profiles():
     g.page_type = "home"
     bg_imgs = randint(1,4)
     return render_template("general/home.html", bg_imgs = bg_imgs)
 
-# create a route and function for the education profile that accepts a CIP id
+@cache.memoize()
+def profile_fetch(attr_type, attr_id):
+    attr_data = attr_cache[attr_type][attr_id]
+    p = Profile(attr_data["id"], attr_type)
+    p.sections()
+    return p
+
 @mod.route("/<attr_type>/<attr_id>/")
 def profile(attr_type, attr_id):
 
@@ -48,8 +54,7 @@ def profile(attr_type, attr_id):
 
 
     # pass id and type to Profile class
-    attr_data = attr_cache[attr_type][attr_id]
-    p = Profile(attr_data["id"], attr_type)
+    p = profile_fetch(attr_type, attr_id)
 
     g.compare = request.args.get("compare", False)
     if g.compare:
@@ -58,9 +63,8 @@ def profile(attr_type, attr_id):
     # render the profile template and pass the profile to jinja
     return render_template("profile/index.html", profile = p)
 
-# create a route and function for the education profile that accepts a CIP id
 @mod.route("/dataloca/<attr_type>/<attr_id>/")
-def profile_dataloca(attr_type, attr_id):
+def profile_dataloca():
     return render_template("profile/dataloca.html")
 
 @mod.route("/stat/")
@@ -77,14 +81,14 @@ def statView():
     return jsonify(stat(args, col=col, dataset=dataset, moe=moe, truncate=truncate))
 
 @mod.route("/<attr_kind>/<attr_id>/img/")
-def splash_img(attr_kind, attr_id, mode="thumb"):
+def splash_img(attr_kind, attr_id):
     return get_img(attr_kind, attr_id, "splash")
 
 @mod.route("/<attr_type>/<attr_id>/<section>/<topic>/")
 def embed_view(attr_type, attr_id, section, topic):
     viz_only = request.args.get("viz", False)
     if not attr_type in PROFILES:
-        abort(404);
+        abort(404)
 
     g.page_class = "{} embed".format(attr_type)
 
