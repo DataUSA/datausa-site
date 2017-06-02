5fb8247ff6a4b95728ca8eef891fefda232c428e
Dave Landry <landry.dave@gmail.com>
2017-06-02 10:55:19 -0400

    bumps cache version

--- datausa/general/views.py
+++ datausa/general/views.py
@@ -16,7 +16,7 @@ mod = Blueprint("general", __name__)
 
 @app.before_request
 def before_request():
-    g.cache_version = 48
+    g.cache_version = 49
     g.cart_limit = 5
     g.affixes = json.dumps(AFFIXES)
     g.colmap = json.dumps(COLMAP)
