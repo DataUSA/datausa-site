1121bfb700ae4379c70f03d1f4a048084ec85ac1
Dave Landry <landry.dave@gmail.com>
2017-06-08 13:44:58 -0400

    creates a prettier and smaller state topojson file

--- scripts/topojson/states/topojson.py
+++ scripts/topojson/states/topojson.py
@@ -1,20 +1,22 @@
-import json, os, subprocess, sys
+import os, sys
 
 def topojson():
 
     directory = "/".join(sys.argv[0].split("/")[:-1])
-    source = "shapefiles/cb_2013_us_state_500k.shp"
-    cmd = "topojson -s 10e-10 --id-property GEOID -o {0}/states.json states={0}/{1}".format(directory, source)
-    print cmd
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p.wait()
+    source = os.path.join(directory, "states/cb_2013_us_state_500k.shp")
 
-    with open("{}/states.json".format(directory), "r+") as data_file:
-        data = json.load(data_file)
-        for geo in data["objects"]["states"]["geometries"]:
-            geo["id"] = "04000US{}".format(geo["id"])
-        data_file.seek(0)
-        json.dump(data, data_file)
+    cmds = []
+
+    cmds.append(("geo2topo -n states=<("
+        "shp2json -n {0} "
+            "| ndjson-map 'd.id = \\\"04000US\\\" + d.properties.GEOID, delete d.properties, d') "
+        "| topoquantize 1e7 "
+        "| toposimplify -p 0.001 -f "
+        "> {1}/states.json"
+        ).format(source, directory))
+
+    os.system("""echo "{}" | pbcopy""".format(" && ".join(cmds)))
+    print "Command copied to clipboard, please press CMD+V to run."
 
 if __name__ == '__main__':
     topojson()
