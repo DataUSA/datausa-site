b9086e6e2ea1da5cfbfbb23240cf015700d69696
Dave Landry <landry.dave@gmail.com>
2017-10-11 10:41:18 -0400

    fixes Collective Learning links in about pages

--- datausa/general/views.py
+++ datausa/general/views.py
@@ -430,7 +430,7 @@ def team():
             "title": "Co-founder",
             "twitter": "https://twitter.com/cesifoti",
             "about": [
-                u"César is the ABC Career Development Professor at the MIT Media Lab and director of the Lab's Macro Connections group. He has ten years of experience in metadata analysis and representation and has been involved in over 30 academic publications exploring its possible uses."
+                u"César is the ABC Career Development Professor at the MIT Media Lab and director of the Lab's Collective Learning group. He has ten years of experience in metadata analysis and representation and has been involved in over 30 academic publications exploring its possible uses."
             ]
         },
         {
