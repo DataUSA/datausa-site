121ba2201a282b4d4543e1b48ab0efff50e9015d
Dave Landry <landry.dave@gmail.com>
2018-01-09 12:07:28 -0500

    updates SAT section

--- datausa/static/js/site.js
+++ datausa/static/js/site.js
@@ -8680,6 +8680,33 @@ viz.bar = function(build) {
 
 }
 
+viz.box = function(build) {
+
+  function getRange(axis) {
+
+    var h = build.viz.height(),
+        w = build.viz.width();
+
+    var max = Math.floor(d3.max([d3.min([w, h])/15, 6]));
+
+    if (build.config[axis]) {
+      var k = build.config[axis].value;
+      if (k !== build.config.id) {
+        var d = axis.indexOf("x") === 0 ? w : h,
+            range = d3.extent(build.viz.data(), function(d) { return d[k]; });
+        range[0] -= range[0] * (max / d);
+        range[1] += range[1] * (max / d);
+        return range;
+      }
+    }
+    return false;
+  }
+
+  return {
+    y: {range: getRange("y")}
+  };
+}
+
 var all_caps = ["cip", "naics", "rca", "soc", "usa"],
     attr_ids = ["geo", "cip", "soc", "naics"],
     range100 = ["non_eng_speakers_pct", "owner_occupied_housing_units", "us_citizens", "grads_total_growth"];
@@ -9017,10 +9044,10 @@ viz.scatter = function(build) {
     return false;
   }
 
-  return {
-    x: {range: getRange("x")},
-    y: {range: getRange("y")}
-  };
+  var retObj = {};
+  if (build.config.x.scale !== "discrete") retObj.x = {range: getRange("x")};
+  if (build.config.y.scale !== "discrete") retObj.y = {range: getRange("y")};
+  return retObj;
 }
 
 viz.tree_map = function(build) {
