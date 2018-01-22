69c0dbb9d8cf919aabd719f0c4a425f6236a9e02
jhmullen <jimmy.mullen@gmail.com>
2018-01-22 18:48:01 -0500

    adds profile route and replace

--- api/mortarRoute.js
+++ api/mortarRoute.js
@@ -5,9 +5,7 @@ module.exports = function(app) {
   const {db} = app.settings;
 
   app.get("/api/profile/:slug/:id", (req, res) => {
-    const {id} = req.params;
-
-    let returnVariables = {};
+    const {slug, id} = req.params;
 
     db.generators.findAll()
       .then(generators => {
@@ -15,8 +13,9 @@ module.exports = function(app) {
         const fetches = requests.map(r => axios.get(r.replace(/\<id\>/g, id)));
         return Promise.all([generators, requests, Promise.all(fetches)]);
       })
-      .then(d => {
-        const [generators, requests, results] = d;
+      .then(resp => {
+        const [generators, requests, results] = resp;
+        let returnVariables = {};
         results.forEach((r, i) => {
           const requiredGenerators = generators.filter(g => g.api === requests[i]);
           requiredGenerators.forEach(g => {
@@ -25,16 +24,40 @@ module.exports = function(app) {
             returnVariables = Object.assign({}, returnVariables, vars);
           });
         });
-        return db.materializers.findAll();
+        return Promise.all([returnVariables, db.materializers.findAll()]);
       })
-      .then(materializers => {
+      .then(resp => {
+        let returnVariables = resp[0];
+        const materializers = resp[1];
         materializers.sort((a, b) => a.ordering - b.ordering);
         materializers.forEach(m => {
           const logicFunc = Function("variables", m.logic);
           const vars = logicFunc(returnVariables);
           returnVariables = Object.assign({}, returnVariables, vars);
         });
-        res.json(returnVariables).end();
+        return Promise.all([returnVariables, db.formatters.findAll()]);
+      })
+      .then(resp => {
+        const [variables, formatters] = resp;
+        const returnObject = {variables};
+        return Promise.all([returnObject, formatters, db.profiles.findOne({where: {slug}, raw: true})]);
+      })
+      .then(resp => {
+        let returnObject = resp[0];
+        const formatters = resp[1];
+        const profile = resp[2];
+        for (const pkey in profile) {
+          if (profile.hasOwnProperty(pkey) && pkey !== "id") {
+            for (const vkey in returnObject.variables) {
+              if (returnObject.variables.hasOwnProperty(vkey)) {
+                const val = returnObject.variables[vkey];
+                profile[pkey] = profile[pkey].replace(`{{${vkey}}}`, val);
+              }
+            }
+          }
+        }
+        returnObject = Object.assign({}, returnObject, profile);
+        res.json(returnObject).end();
       })
       .catch(err => {
         console.error(err);
