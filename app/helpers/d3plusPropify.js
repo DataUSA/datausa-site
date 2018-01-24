503069d0bf8d35c943d23c1bdad08cd79a2f1e85
Dave Landry <landry.dave@gmail.com>
2018-01-24 16:19:52 -0500

    adds dataFold and raw catches to default dataFormat for visualizations

--- app/helpers/d3plusPropify.js
+++ app/helpers/d3plusPropify.js
@@ -1,4 +1,5 @@
 import {assign} from "d3plus-common";
+import {dataFold} from "d3plus-viz";
 import cubeFold from "./cubeFold";
 
 export default config => {
@@ -7,7 +8,13 @@ export default config => {
 
   // strip out the "dataFormat" from config
   let {dataFormat} = configClone;
-  if (!dataFormat) dataFormat = data => cubeFold(data).data;
+  if (!dataFormat) {
+    dataFormat = data => {
+      if (data.axes && data.values) return cubeFold(data).data;
+      if (data.headers && data.data) return dataFold(data);
+      return data;
+    };
+  }
   delete configClone.dataFormat;
 
   // hides the non-discrete axis, if necessary
