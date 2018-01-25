0f7b5c001defe1df521d8e3e17216cc668c5969f
Dave Landry <landry.dave@gmail.com>
2018-01-25 10:37:23 -0500

    displays merged Cart data in blueprint table

--- app/pages/Cart.jsx
+++ app/pages/Cart.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 
+import {Cell, Column, Table} from "@blueprintjs/table";
+import "@blueprintjs/table/dist/table.css";
 import axios from "axios";
 import {merge} from "d3-array";
 import {nest} from "d3-collection";
@@ -45,19 +47,36 @@ export default class Cart extends Component {
         const stickies = Array.from(new Set(merge(foldedData.map(d => Object.keys(d.dimensions)))));
         const results = nest()
           .key(d => stickies.map(key => d[key] || "undefined").join("-"))
-          .entries(merge(foldedData.map(d => d.data)));
+          .rollup(leaves => Object.assign(...leaves))
+          .entries(merge(foldedData.map(d => d.data)))
+          .map(d => d.value);
         this.setState({columns, results, stickies, values});
       })
       .catch(err => console.error(err));
   }
 
   render() {
-    const {results} = this.state;
+    const {columns, results, stickies} = this.state;
+    console.log(columns);
+    console.log(stickies);
     console.log(results);
+    const headers = Object.keys(columns)
+      .sort((a, b) => {
+        const sA = stickies.indexOf(a);
+        const sB = stickies.indexOf(b);
+        return sA < 0 && sB < 0 ? a.localeCompare(b) : sB - sA;
+      });
+
+    const renderCell = (rowIndex, columnIndex) => {
+      return <Cell>{ results[rowIndex][headers[columnIndex]] }</Cell>;
+    };
+
     return (
       <div id="Cart">
         { !results ? "Loading" : <div>
-          Data!
+          <Table numRows={ results.length }>
+            { headers.map(c => <Column id={ c } key={ c } name={ c } renderCell={ renderCell } />) }
+          </Table>
         </div> }
       </div>
     );
