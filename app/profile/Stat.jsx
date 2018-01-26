4adb334c2a24681a0a7a4ffeeb73bcabdaf93a94
Dave Landry <landry.dave@gmail.com>
2018-01-26 15:28:29 -0500

    uses dangerouslySetInnerHTML in Stat

--- app/profile/Stat.jsx
+++ app/profile/Stat.jsx
@@ -5,9 +5,9 @@ export default class Stat extends Component {
   render() {
     const {subtitle, title, value} = this.props.data;
     return <div className="Stat">
-      <div className="stat-title">{ title }</div>
-      <div className="stat-value">{ value }</div>
-      <div className="stat-subtitle">{ subtitle }</div>
+      <div className="stat-title" dangerouslySetInnerHTML={{__html: title}} />
+      <div className="stat-value" dangerouslySetInnerHTML={{__html: value}} />
+      <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: subtitle}} />
     </div>;
   }
 
