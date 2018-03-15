ddec11184a3bde9cd896b2f96ce09d6b66108376
Dave Landry <landry.dave@gmail.com>
2018-03-14 22:13:08 -0500

    implements DataSource page

---
+++ app/pages/Data/index.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import SubNav from "components/SubNav/index";
+import NavLink from "components/NavLink";
+
+import "../About/index.css";
+
+class Data extends Component {
+
+  getChildContext() {
+    return {
+      location: this.props.location
+    };
+  }
+
+  render() {
+
+    return (
+      <div id="Data">
+        <SubNav>
+          <NavLink to="/about/datasets/">Data Sources</NavLink>
+          <NavLink to="/about/api/">API</NavLink>
+          <NavLink to="/about/attributes/">Classifications</NavLink>
+        </SubNav>
+        { this.props.children }
+      </div>
+    );
+
+  }
+
+}
+
+Data.childContextTypes = {
+  location: PropTypes.object
+};
+
+export default Data;
