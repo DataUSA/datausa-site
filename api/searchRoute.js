1caa208f4bc138dfd7ec42d51845f5fa9a8b4991
Dave Landry <landry.dave@gmail.com>
2018-03-14 21:40:58 -0500

    added more test search results

--- api/searchRoute.js
+++ api/searchRoute.js
@@ -8,6 +8,18 @@ module.exports = function(app) {
           name: "Massachusetts",
           sumlevel: "State",
           type: "geo"
+        },
+        {
+          id: "24",
+          name: "Banking",
+          sumlevel: "Industry Group",
+          type: "naics"
+        },
+        {
+          id: "04000US34",
+          name: "New York, NY",
+          sumlevel: "Place",
+          type: "geo"
         }
       ]
     }).end();
---
+++ app/toCanon/slugify.js
@@ -0,0 +1,2 @@
+import {strip} from "d3plus-text";
+export default title => strip(title).toLowerCase();
