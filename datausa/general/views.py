53e2de8b2648f28750686215f5f290f9bd91393d
Dave Landry <landry.dave@gmail.com>
2018-04-13 13:08:17 -0400

    adds max-height to profile visualizations relative to screen height

--- datausa/general/views.py
+++ datausa/general/views.py
@@ -16,7 +16,7 @@ mod = Blueprint("general", __name__)
 
 @app.before_request
 def before_request():
-    g.cache_version = 62
+    g.cache_version = 63
     g.cart_limit = 5
     g.affixes = json.dumps(AFFIXES)
     g.collectionyears = json.dumps(COLLECTIONYEARS)
