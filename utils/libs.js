bde436ccc78d8cb1edb7998b87bc0d7463283bbf
Dave Landry <landry.dave@gmail.com>
2018-04-26 12:56:46 -0400

    adds d3-format to formatter libs

--- utils/libs.js
+++ utils/libs.js
@@ -1,7 +1,8 @@
-const d3TimeFormat = require("d3-time-format"),
+const d3Format = require("d3-format"),
+      d3TimeFormat = require("d3-time-format"),
       d3plusAxis = require("d3plus-axis");
 
 module.exports = {
-  d3: Object.assign({}, d3TimeFormat),
+  d3: Object.assign({}, d3Format, d3TimeFormat),
   d3plus: Object.assign({}, d3plusAxis)
 };
