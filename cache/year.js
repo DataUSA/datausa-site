fa17d793b2364719c95e6421086aa24bbf432695
Dave Landry <landry.dave@gmail.com>
2018-05-03 17:16:38 -0400

    formatting of cube year queries

--- cache/year.js
+++ cache/year.js
@@ -1,8 +1,32 @@
 const axios = require("axios");
 
+const {CUBE_URL} = process.env;
+
 module.exports = function() {
 
-  return axios.get("http://datausa-cube.datawheel.us:5000/cubes/")
-    .then(d => d.data);
+  return axios.get(`${CUBE_URL}/cubes/`)
+    .then(resp => resp.data.cubes)
+    .then(cubes => {
+
+      const cubeQueries = cubes
+        .filter(cube => cube.dimensions.find(d => d.name === "Year"))
+        .map(cube => axios
+          .get(`${CUBE_URL}cubes/${cube.name}/aggregate.jsonrecords?drilldown[]=[Year].[Year]`)
+          .then(resp => {
+            const years = resp.data.map(d => d["ID Year"]).sort();
+            return {
+              cube: cube.name,
+              latest: years[years.length - 1],
+              oldest: years[0],
+              years
+            };
+          })
+          .catch(() => console.log("Error: ", cube.name)));
+
+      return Promise.all(cubeQueries)
+        .then(arr => arr.filter(d => d)
+          .reduce((obj, d) => (obj[d.cube] = d, obj), {}));
+
+    });
 
 };
