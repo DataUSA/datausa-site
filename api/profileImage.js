5fc54502ce3c6f89761a8f0bed6bbc7a5de9942f
Dave Landry <landry.dave@gmail.com>
2018-05-16 16:38:51 -0400

    uses geoservice-api for parent images

--- api/profileImage.js
+++ api/profileImage.js
@@ -1,4 +1,7 @@
-const slugMap = require("../utils/slugMap");
+const axios = require("axios"),
+      slugMap = require("../utils/slugMap");
+
+const {CUBE_URL} = process.env;
 
 module.exports = function(app) {
 
@@ -18,38 +21,56 @@ module.exports = function(app) {
 
         if (!imageId) {
 
-          const type = slugMap[pslug];
+          if (pslug === "geo") {
 
-          client.cube("acs_yg_total_population_5")
-            .then(cube => {
+            axios.get(`${CUBE_URL}geoservice-api/relations/parents/${attr.id}`)
+              .then(d => d.data.reverse())
+              .then(d => d.map(p => p.geoid))
+              .then(d => Promise.all([d, db.search.findAll({where: {id: d, type: "geo"}})]))
+              .then(([ids, parents]) => {
+                const parentImage = parents
+                  .sort((a, b) => ids.indexOf(a.id) - ids.indexOf(b.id))
+                  .find(p => p.imageId).imageId;
+                sendImage(parentImage);
+              });
 
-              const query = cube.query
-                .drilldown(type, sumlevel, sumlevel)
-                .cut(`[${type}].[${sumlevel}].[${sumlevel}].&[${pid}]`)
-                .option("parents", true);
+          }
+          else {
 
-              return client.query(query);
+            const type = slugMap[pslug];
 
-            })
-            .then(d => d.data)
-            .then(data => {
+            client.cube("acs_yg_total_population_5")
+              .then(cube => {
 
-              const hierarchy = data.axes
-                .find(d => d.name === type);
+                const query = cube.query
+                  .drilldown(type, sumlevel, sumlevel)
+                  .cut(`[${type}].[${sumlevel}].[${sumlevel}].&[${attr.id}]`)
+                  .option("parents", true);
 
-              const ancestors = hierarchy.members[0].ancestors
-                .filter(d => d.level_name !== "(All)");
+                return client.query(query);
 
-              const parentIds = ancestors.map(d => d.key).reverse();
-              console.log(parentIds, pslug);
-              return db.search.findAll({where: {id: parentIds, type: pslug}, raw: true});
+              })
+              .then(d => d.data)
+              .then(data => {
 
-            })
-            .then(parents => {
-              const firstImage = parents.find(d => d.imageId);
-              if (firstImage && firstImage.imageId) sendImage(firstImage.imageId);
-              else sendImage(imageId);
-            });
+                const hierarchy = data.axes
+                  .find(d => d.name === type);
+
+                const ancestors = hierarchy.members[0].ancestors
+                  .filter(d => d.level_name !== "(All)");
+
+                const parentIds = ancestors.map(d => d.key).reverse();
+                console.log(parentIds, pslug);
+                return db.search.findAll({where: {id: parentIds, type: pslug}, raw: true});
+
+              })
+              .then(parents => {
+                const firstImage = parents.find(d => d.imageId);
+                if (firstImage && firstImage.imageId) sendImage(firstImage.imageId);
+                else sendImage(imageId);
+              });
+
+          }
         }
         else sendImage(imageId);
       });
