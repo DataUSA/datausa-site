3f56e95bcb752d4d695b0b32b2f4f73eeedc2704
Dave Landry <landry.dave@gmail.com>
2018-05-18 14:18:46 -0400

    adds d3-array and d3-collection to available CMS libs

--- utils/libs.js
+++ utils/libs.js
@@ -1,8 +1,10 @@
-const d3Format = require("d3-format"),
+const d3Array = require("d3-array"),
+      d3Collection = require("d3-collection"),
+      d3Format = require("d3-format"),
       d3TimeFormat = require("d3-time-format"),
       d3plusAxis = require("d3plus-axis");
 
 module.exports = {
-  d3: Object.assign({}, d3Format, d3TimeFormat),
+  d3: Object.assign({}, d3Array, d3Collection, d3Format, d3TimeFormat),
   d3plus: Object.assign({}, d3plusAxis)
 };
