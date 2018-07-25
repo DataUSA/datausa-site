499e6dd02510e7d4ea2568914946da26ae94d5de
Dave Landry <landry.dave@gmail.com>
2018-07-25 17:07:16 -0400

    removes trailing comma from ConsoleVariable array display

--- app/admin/components/ConsoleVariable.jsx
+++ app/admin/components/ConsoleVariable.jsx
@@ -23,7 +23,7 @@ export default class ConsoleVariable extends Component {
     else if (t === "error") v = `Error: ${v.message}`;
     else if (t === "undefined") v = t;
     else if (t === "array") {
-      v = <span>[{v.map((l, i) => <span key={i}><ConsoleVariable value={l} />, </span>)}]</span>;
+      v = <span>[{v.map((l, i) => <span key={i}><ConsoleVariable value={l} />{ i < v.length - 1 ? ", " : "" }</span>)}]</span>;
     }
     else if (v.toString) v = v.toString();
 
