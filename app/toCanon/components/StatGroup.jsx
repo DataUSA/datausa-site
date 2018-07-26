db9f9fe76ec4aeb054a67fdab083bfc5149c514c
Dave Landry <landry.dave@gmail.com>
2018-07-26 16:48:17 -0400

    implements StatGroup component for profiles

---
+++ app/toCanon/components/StatGroup.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import "./StatGroup.css";
+
+export default class StatGroup extends Component {
+
+  render() {
+    const {stats, title} = this.props;
+
+    return <div className="StatGroup">
+      { title && stats.length > 1 && <div className="stat-title" dangerouslySetInnerHTML={{__html: title}}></div> }
+      { stats.length > 1 && <ol>
+        { stats.map((stat, i) => <li key={i}>
+          <div className="stat-value" dangerouslySetInnerHTML={{__html: stat.value}}></div>
+          <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: stat.subtitle}}></div>
+        </li>) }
+      </ol> }
+      { stats.length === 1 && <div className="stat-value" dangerouslySetInnerHTML={{__html: stats[0].value}}></div> }
+      { title && stats.length === 1 && <div className="stat-title" dangerouslySetInnerHTML={{__html: title}}></div> }
+      { stats.length === 1 && <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: stats[0].subtitle}}></div> }
+    </div>;
+  }
+
+}
