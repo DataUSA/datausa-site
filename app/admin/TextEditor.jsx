ec016dc51e62d337fbee158ca7644a942ea1d3e1
Dave Landry <landry.dave@gmail.com>
2018-07-26 16:47:51 -0400

    saves empty TextEditor contents as an empty string

--- app/admin/TextEditor.jsx
+++ app/admin/TextEditor.jsx
@@ -32,6 +32,7 @@ class TextEditor extends Component {
 
   handleEditor(field, t) {
     const {data} = this.state;
+    if (t === "<p><br></p>") t = "";
     data[field] = t;
     this.setState({data});
   }
