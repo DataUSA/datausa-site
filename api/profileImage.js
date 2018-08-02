c91dab8afce7ac0b040d7a1fdd5fcfdc56524f7c
Dave Landry <landry.dave@gmail.com>
2018-08-02 16:22:11 -0400

    fixes profileImage endpoint for non-geo images

--- api/profileImage.js
+++ api/profileImage.js
@@ -13,69 +13,56 @@ const slugMap = {
 module.exports = function(app) {
 
   const {db} = app.settings;
-  const {client} = app.settings.cache.cube;
+  const {parents} = app.settings.cache;
 
   app.get("/api/profile/:pslug/:pid/:size", (req, res) => {
     const {size, pid, pslug} = req.params;
 
     function sendImage(image) {
-      res.sendFile(`${process.cwd()}/static/images/profile/${size}/${image}.jpg`);
+      if (image) res.sendFile(`${process.cwd()}/static/images/profile/${size}/${image}.jpg`);
+      else if (pslug === "university") res.sendFile(`${process.cwd()}/static/images/profile/${size}/2032.jpg`);
+      else res.sendFile(`${process.cwd()}/static/images/profile/${size}/1849.jpg`);
     }
 
     db.search.findOne({where: {id: pid, dimension: slugMap[pslug]}})
       .then(attr => {
-        const {dimension, imageId, hierarchy} = attr;
+
+        const {imageId} = attr;
 
         if (!imageId) {
 
-          if (pslug === "geo") {
+          if (parents[pslug]) {
 
-            axios.get(`${CUBE_URL}geoservice-api/relations/parents/${attr.id}`)
-              .then(d => d.data.reverse())
-              .then(d => d.map(p => p.geoid))
-              .then(d => Promise.all([d, db.search.findAll({where: {id: d, type: "geo"}})]))
-              .then(([ids, parents]) => {
-                const parentImage = parents
+            const ids = parents[pslug][pid];
+
+            db.search.findAll({where: {id: ids, dimension: slugMap[pslug]}})
+              .then(parentAttrs => {
+                const parentImage = parentAttrs
                   .sort((a, b) => ids.indexOf(a.id) - ids.indexOf(b.id))
                   .find(p => p.imageId).imageId;
                 sendImage(parentImage);
               });
 
           }
-          else {
+          else if (pslug === "geo") {
 
-            client.cube("acs_yg_total_population_5")
-              .then(cube => {
-
-                const query = cube.query
-                  .drilldown(dimension, hierarchy, hierarchy)
-                  .cut(`[${dimension}].[${hierarchy}].[${hierarchy}].&[${attr.id}]`)
-                  .option("parents", true);
-
-                return client.query(query);
-
+            axios.get(`${CUBE_URL}geoservice-api/relations/parents/${attr.id}`)
+              .then(d => d.data.reverse())
+              .then(d => d.map(p => p.geoid))
+              .then(d => {
+                const attrs = db.search.findAll({where: {id: d, dimension: slugMap[pslug]}});
+                return Promise.all([d, attrs]);
               })
-              .then(d => d.data)
-              .then(data => {
-
-                const hierarchy = data.axes
-                  .find(d => d.name === dimension);
-
-                const ancestors = hierarchy.members[0].ancestors
-                  .filter(d => d.level_name !== "(All)");
-
-                const parentIds = ancestors.map(d => d.key).reverse();
-                console.log(parentIds, pslug);
-                return db.search.findAll({where: {id: parentIds, dimension: pslug}, raw: true});
-
-              })
-              .then(parents => {
-                const firstImage = parents.find(d => d.imageId);
-                if (firstImage && firstImage.imageId) sendImage(firstImage.imageId);
-                else sendImage(imageId);
+              .then(([ids, parentAttrs]) => {
+                const parentImage = parentAttrs
+                  .sort((a, b) => ids.indexOf(a.id) - ids.indexOf(b.id))
+                  .find(p => p.imageId).imageId;
+                sendImage(parentImage);
               });
 
           }
+          else sendImage();
+
         }
         else sendImage(imageId);
       });
