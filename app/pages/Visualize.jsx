623d893ee637d5001934fd39f6393d108f3fafea
Dave Landry <landry.dave@gmail.com>
2018-08-09 12:49:34 -0400

    enables Vizbuilder topojson

--- app/pages/Visualize.jsx
+++ app/pages/Visualize.jsx
@@ -6,6 +6,22 @@ import "./Visualize.css";
 
 export default class Visualize extends React.Component {
   render() {
-    return <Vizbuilder src="https://canon-api.datausa.io" />;
+    return <Vizbuilder src="https://canon-api.datausa.io"
+      topojson={{
+        County: {
+          topojson: "topojson/County.json"
+        },
+        Msa: {
+          topojson: "topojson/Msa.json"
+        },
+        Puma: {
+          topojson: "topojson/Puma.json"
+        },
+        State: {
+          projection: "geoAlbersUsa",
+          ocean: "transparent",
+          topojson: "topojson/State.json"
+        }
+      }} />;
   }
 }
