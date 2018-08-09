6641a117e993abba4c889a5c9af454b2ba1bd10a
Dave Landry <landry.dave@gmail.com>
2018-08-09 01:38:34 -0400

    adds plus-minus styling to StatGroup

--- app/toCanon/components/StatGroup.jsx
+++ app/toCanon/components/StatGroup.jsx
@@ -1,22 +1,24 @@
 import React, {Component} from "react";
 import "./StatGroup.css";
 
+const sanitize = html => html.replace(/\±/g, "<span class='plus-minus'>±</span>");
+
 export default class StatGroup extends Component {
 
   render() {
     const {stats, title} = this.props;
 
     return <div className={ `StatGroup ${stats.length > 1 ? "multi" : "single"}` }>
-      { title && stats.length > 1 && <div className="stat-title" dangerouslySetInnerHTML={{__html: title}}></div> }
+      { title && stats.length > 1 && <div className="stat-title" dangerouslySetInnerHTML={{__html: sanitize(title)}}></div> }
       { stats.length > 1 && <ol>
         { stats.map((stat, i) => <li key={i}>
-          <div className="stat-value" dangerouslySetInnerHTML={{__html: stat.value}}></div>
-          <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: stat.subtitle}}></div>
+          <div className="stat-value" dangerouslySetInnerHTML={{__html: sanitize(stat.value)}}></div>
+          <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: sanitize(stat.subtitle)}}></div>
         </li>) }
       </ol> }
-      { stats.length === 1 && <div className="stat-value" dangerouslySetInnerHTML={{__html: stats[0].value}}></div> }
-      { title && stats.length === 1 && <div className="stat-title" dangerouslySetInnerHTML={{__html: title}}></div> }
-      { stats.length === 1 && <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: stats[0].subtitle}}></div> }
+      { stats.length === 1 && <div className="stat-value" dangerouslySetInnerHTML={{__html: sanitize(stats[0].value)}}></div> }
+      { title && stats.length === 1 && <div className="stat-title" dangerouslySetInnerHTML={{__html: sanitize(title)}}></div> }
+      { stats.length === 1 && <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: sanitize(stats[0].subtitle)}}></div> }
     </div>;
   }
 
