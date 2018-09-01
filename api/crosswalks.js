22e2002adaea79f8f40428aee25a72e13a833261
Dave Landry <landry.dave@gmail.com>
2018-09-01 18:46:12 -0300

    adds CIP parent API endpoint

--- api/crosswalks.js
+++ api/crosswalks.js
@@ -8,6 +8,20 @@ module.exports = function(app) {
 
   const {db} = app.settings;
 
+  app.get("/api/cip/parent/:id/:level", (req, res) => {
+
+    const {id, level} = req.params;
+    const depth = parseInt(level.slice(3), 10);
+    const parentId = id.slice(0, depth);
+    db.search
+      .findOne({where: {id: parentId, dimension: "CIP"}})
+      .then(cip => {
+        res.json(cip);
+      })
+      .catch(err => res.json(err));
+
+  });
+
   app.get("/api/university/similar/:id", (req, res) => {
 
     const ids = similarities[req.params.id] || [];
@@ -15,9 +29,9 @@ module.exports = function(app) {
     db.search
       .findAll({where: {id: ids, dimension: "University"}})
       .then(universities => {
-        res.json(universities).end();
+        res.json(universities);
       })
-      .catch(err => res.json(err).end());
+      .catch(err => res.json(err));
 
   });
 
