2ad6e7a745fc091239a32558aa4b8beb913ba576
Dave Landry <landry.dave@gmail.com>
2018-09-06 17:14:01 -0400

    moves section icons to their own folder

--- app/toCanon/SectionIcon.jsx
+++ app/toCanon/SectionIcon.jsx
@@ -6,7 +6,7 @@ export default class SectionIcon extends Component {
   render() {
     const {active, slug, title} = this.props;
     return <AnchorLink to={ slug } className={ `SectionIcon ${active ? "active" : "" }` }>
-      <img src={ `/images/icons/${slug}.svg` } />
+      <img src={ `/sections/${slug}.svg` } />
       <div dangerouslySetInnerHTML={{__html: title}}></div>
     </AnchorLink>;
   }
