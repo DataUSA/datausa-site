ce2d48d6124af887d66cd22087f3b081ba8f2cfc
Dave Landry <landry.dave@gmail.com>
2018-10-22 13:32:13 -0400

    allows urlSwap to use CANON_LOGICLAYER_CUBE

--- utils/urlSwap.js
+++ utils/urlSwap.js
@@ -1,8 +1,16 @@
+const {CANON_LOGICLAYER_CUBE} = process.env;
+
+const env = {
+  CANON_LOGICLAYER_CUBE
+};
+
 module.exports = function(url, params) {
+
+  const lookup = Object.assign(env, params);
 
   (url.match(/<[^\&\=\/>]+>/g) || []).forEach(variable => {
     let x = variable.slice(1, -1).split(".");
-    if (params[x[0]]) x = x.reduce((o, i) => o[i], params);
+    if (lookup[x[0]]) x = x.reduce((o, i) => o[i], lookup);
     else x = false;
     if (x && typeof x !== "object") url = url.replace(variable, x);
   });
