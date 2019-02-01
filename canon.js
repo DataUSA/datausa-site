7047311eae9b58112aefb70925a4c1ea0b2d0ad2
Dave Landry <landry.dave@gmail.com>
2019-02-01 11:39:14 -0500

    fixes C table logic (closes #798)

--- canon.js
+++ canon.js
@@ -24,23 +24,11 @@ module.exports = {
           const tracts = query.dimensions.filter(d => (d.relation || "").includes("Tract")).length;
           return cubes.filter(cube => cube.name.match(bigGeos && !tracts ? /_1$/g : /_5$/g));
         },
-        key: cube => cube.name.replace(/_[0-9]$/g, "")
+        key: cube => cube.name.replace("_c_", "_").replace(/_[0-9]$/g, "")
       },
       {
         filter: cubes => cubes.filter(c => c.name.includes("_c_")),
         key: cube => cube.name.replace("_c_", "_")
-      },
-      {
-        filter: (cubes, query, caches) => {
-          const {pops} = caches;
-          const ids = d3Array.merge(query.dimensions
-            .filter(d => d.dimension === "Geography")
-            .map(d => d.id instanceof Array ? d.id : [d.id]));
-          const bigGeos = ids.every(g => pops[g] && pops[g] >= 250000);
-          const tracts = query.dimensions.filter(d => (d.relation || "").includes("Tract")).length;
-          return cubes.filter(cube => bigGeos && !tracts ? cube.name.includes("_c_") : !cube.name.includes("_c_"));
-        },
-        key: cube => cube.name.replace("_c_", "_").replace(/_[0-9]$/g, "")
       }
     ],
     dimensionMap: {
