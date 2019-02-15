19db9841bf697fd19f90cd2c541b840f1e136c14
Dave Landry <landry.dave@gmail.com>
2019-02-15 15:50:25 -0500

    collapses data sources page into accordions

--- app/pages/Data/DataSources.jsx
+++ app/pages/Data/DataSources.jsx
@@ -1,15 +1,27 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {fetchData} from "@datawheel/canon-core";
-
-import Anchor from "components/Anchor";
+import {Button, Collapse} from "@blueprintjs/core";
 import {caveats, usage} from "./datasets.js";
 
 class DataSources extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {active: []};
+  }
+
+  onClick(slug) {
+    const {active} = this.state;
+    if (active.includes(slug)) active.splice(active.indexOf(slug), 1);
+    else active.push(slug);
+    this.setState({active});
+  }
+
   render() {
 
     const {datasets} = this.props;
+    const {active} = this.state;
 
     return (
       <div id="DataSources">
@@ -29,17 +41,25 @@ class DataSources extends Component {
           const use = usage[slug] || [];
           const caveat = caveats[slug] || [];
 
+          const open = active.includes(slug);
+
           return <div key={title}>
-            <h2 id={slug}><Anchor slug={slug}>{ title }</Anchor></h2>
-            { desc.map((d, i) => <p key={i} dangerouslySetInnerHTML={{__html: d}} />) }
-            { use.length > 0 && <h3>Where is it used?</h3> }
-            { use.length > 0 && use.map((d, i) => <p key={i} dangerouslySetInnerHTML={{__html: d}} />) }
-            { caveat.length > 0 && <h3>Caveats</h3> }
-            { caveat.length > 0 && caveat.map((d, i) => <p key={i} dangerouslySetInnerHTML={{__html: d}} />) }
-            { datasets.length > 0 && <h3>Dataset{ datasets.length > 1 ? "s" : "" }</h3> }
-            { datasets.length > 0 && <ul>
-              { datasets.map((d, i) => <li key={i}><a href={d.link} target="_blank" rel="noopener noreferrer">{d.title}</a></li>) }
-            </ul> }
+            <Button
+              active={open}
+              className="pt-fill"
+              rightIconName={open ? "chevron-down" : "chevron-right"}
+              onClick={this.onClick.bind(this, slug)}>{ title }</Button>
+            <Collapse isOpen={open} keepChildrenMounted={true}>
+              { desc.map((d, i) => <p key={i} dangerouslySetInnerHTML={{__html: d}} />) }
+              { use.length > 0 && <div className="sub-title">Where is it used?</div> }
+              { use.length > 0 && use.map((d, i) => <p key={i} dangerouslySetInnerHTML={{__html: d}} />) }
+              { caveat.length > 0 && <div className="sub-title">Caveats</div> }
+              { caveat.length > 0 && caveat.map((d, i) => <p key={i} dangerouslySetInnerHTML={{__html: d}} />) }
+              { datasets.length > 0 && <div className="sub-title">Source Link{ datasets.length > 1 ? "s" : "" }</div> }
+              { datasets.length > 0 && <ul>
+                { datasets.map((d, i) => <li key={i}><a href={d.link} target="_blank" rel="noopener noreferrer">{d.title}</a></li>) }
+              </ul> }
+            </Collapse>
           </div>;
 
         }) }
