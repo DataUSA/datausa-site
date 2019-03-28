e9a30c4a653f06b4b1f2a27ec06c04e58307af02
Dave Landry <landry.dave@gmail.com>
2019-03-28 13:29:29 -0400

    improves Loading component to show progress

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -7,11 +7,51 @@ import "./Loading.css";
   loaded into the redux store.
 */
 export default class Loading extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      interval: false,
+      progress: -1
+    };
+  }
+
+  componentDidUpdate() {
+    const {progress, total} = this.props;
+    const mod = 0.02;
+    if (progress === total) {
+      const {interval} = this.state;
+      if (interval) clearInterval(interval);
+    }
+    else if (progress > this.state.progress) {
+      const {interval} = this.state;
+      if (interval) clearInterval(interval);
+      let tempProg = progress;
+      const newInterval = setInterval(() => {
+        if (tempProg + mod < Math.floor(progress) + 1) {
+          tempProg += mod;
+          this.setState({progress: tempProg});
+        }
+        else {
+          clearInterval(newInterval);
+        }
+      }, 100);
+      this.setState({progress, interval: newInterval});
+    }
+  }
+
   render() {
+
+    const {total} = this.props;
+    const {progress} = this.state;
+
+    const ratio = total ? (progress || 0) / total : 0;
+
     return <NonIdealState
       className="loading"
       title="Loading"
       description="Please Wait"
-      visual={<Spinner />} />;
+      visual={<Spinner value={ total && total !== progress && ratio > 0.05 ? ratio : undefined }/>} />;
   }
+
 }
