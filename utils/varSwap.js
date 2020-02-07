c6a2b94925e61dc51c9a96e41da04ced8189200f
Dave Landry <landry.dave@gmail.com>
2020-02-07 16:06:38 -0500

    adds try/catch to varSwap logic

--- utils/varSwap.js
+++ utils/varSwap.js
@@ -6,7 +6,7 @@ const libs = require("./libs");
  * TODO: maybe make this recursive in the future, crawling down the object?
 */
 
-module.exports = (sourceString, formatterFunctions, variables) => {  
+module.exports = (sourceString, formatterFunctions, variables) => {
   // Find all instances of the following type:  FormatterName{{VarToReplace}}
   sourceString = sourceString.replace(/([A-z0-9]*)\{\{([^\}]+)\}\}/g, (match, g1, keyMatch) => {
 
@@ -18,8 +18,21 @@ module.exports = (sourceString, formatterFunctions, variables) => {
     }
 
     const value = variables[keyMatch];
-    if (value === undefined) return "N/A";
-    else return formatter(value, libs, formatterFunctions);
+    if (value === undefined) {
+      return "N/A";
+    }
+    // The user-created formatter may be malformed. Wrap in a try/catch so bad js in a
+    // formatter doesn't cause the CMS to crash.
+    else {
+      try {
+        return formatter(value, libs, formatterFunctions);
+      }
+      catch (e) {
+        console.error(`Error using formatter ${formatter.name}`);
+        console.error(`Error message: ${e.message}`);
+        return "N/A";
+      }
+    }
 
   });
   return sourceString;
