46e89e19a6933e14e7a697e4d17a56e771ed5062
Dave Landry <landry.dave@gmail.com>
2020-02-26 17:40:19 -0500

    improves profile stat text resizing based on value

--- app/toCanon/Stat.jsx
+++ app/toCanon/Stat.jsx
@@ -4,7 +4,8 @@ export default class Stat extends Component {
 
   render() {
     const {subtitle, title, value} = this.props.data;
-    return <div className={`Stat ${ value.length - 7 > 10 ? "small-text" : "large-text" }`}>
+    const l = value.length - 7;
+    return <div className={`Stat ${ l > 15 ? "small" : l > 10 ? "medium" : "large" }-text`}>
       <div className="stat-title" dangerouslySetInnerHTML={{__html: title}} />
       <div className="stat-value" dangerouslySetInnerHTML={{__html: value}} />
       <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: subtitle}} />
