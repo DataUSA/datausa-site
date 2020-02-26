de58f35e3097f03cb66ae30db64d278bf1e0ac33
Dave Landry <landry.dave@gmail.com>
2020-02-26 17:23:15 -0500

    decreases large word cutoff in Stat renderer

--- app/toCanon/Stat.jsx
+++ app/toCanon/Stat.jsx
@@ -4,7 +4,7 @@ export default class Stat extends Component {
 
   render() {
     const {subtitle, title, value} = this.props.data;
-    return <div className={`Stat ${ value.length - 7 > 15 ? "small-text" : "large-text" }`}>
+    return <div className={`Stat ${ value.length - 7 > 10 ? "small-text" : "large-text" }`}>
       <div className="stat-title" dangerouslySetInnerHTML={{__html: title}} />
       <div className="stat-value" dangerouslySetInnerHTML={{__html: value}} />
       <div className="stat-subtitle" dangerouslySetInnerHTML={{__html: subtitle}} />
