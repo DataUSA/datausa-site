22dec0b14384e4b3d21fcbbf263f45e14f7d7965
Dave Landry <landry.dave@gmail.com>
2020-02-28 17:19:08 -0500

    adds candidate image CSS styles

--- app/toCanon/Stat.jsx
+++ app/toCanon/Stat.jsx
@@ -3,8 +3,13 @@ import React, {Component} from "react";
 export default class Stat extends Component {
 
   render() {
-    const {subtitle, title, value} = this.props.data;
-    const l = value.length - 7;
+    const {subtitle, title} = this.props.data;
+    let {value} = this.props.data;
+    let l = value.length - 7;
+    if (value.includes("candidate-image")) {
+      l += 5;
+      value = value.replace("</span></span>", "</span></span></span><span class='candidate-name'>").replace("</p>", "</span></p>");
+    }
     return <div className={`Stat ${ l > 15 ? "small" : l > 10 ? "medium" : "large" }-text`}>
       <div className="stat-title" dangerouslySetInnerHTML={{__html: title}} />
       <div className="stat-value" dangerouslySetInnerHTML={{__html: value}} />
