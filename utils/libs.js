5d8ad87b172bd77b41c60512e547859ba497b1b7
Dave Landry <landry.dave@gmail.com>
2020-03-05 17:42:19 -0500

    adds mroe d3plus libraries to libs

--- utils/libs.js
+++ utils/libs.js
@@ -3,9 +3,10 @@ const d3Array = require("d3-array"),
       d3Format = require("d3-format"),
       d3TimeFormat = require("d3-time-format"),
       {date} = require("d3plus-axis"),
-      {assign, closest, merge} = require("d3plus-common");
+      {assign, closest, merge} = require("d3plus-common"),
+      {strip, titleCase} = require("d3plus-text");
 
 module.exports = {
   d3: Object.assign({}, d3Array, d3Collection, d3Format, d3TimeFormat),
-  d3plus: {assign, closest, date, merge}
+  d3plus: {assign, closest, date, merge, strip, titleCase}
 };
