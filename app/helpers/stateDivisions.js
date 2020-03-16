37f2def8250d38b06a1c375ab687241b08256701
Dave Landry <landry.dave@gmail.com>
2020-03-16 18:00:23 -0400

    first pass at Coronavirus page

---
+++ app/helpers/stateDivisions.js
@@ -0,0 +1,116 @@
+export const stateToDivision = {
+  "04000US01": 6,
+  "04000US02": 9,
+  "04000US04": 8,
+  "04000US05": 7,
+  "04000US06": 9,
+  "04000US08": 8,
+  "04000US09": 1,
+  "04000US10": 5,
+  "04000US11": 5,
+  "04000US12": 5,
+  "04000US13": 5,
+  "04000US15": 9,
+  "04000US16": 8,
+  "04000US17": 3,
+  "04000US18": 3,
+  "04000US19": 4,
+  "04000US20": 4,
+  "04000US21": 6,
+  "04000US22": 7,
+  "04000US23": 1,
+  "04000US24": 5,
+  "04000US25": 1,
+  "04000US26": 3,
+  "04000US27": 4,
+  "04000US28": 6,
+  "04000US29": 4,
+  "04000US30": 8,
+  "04000US31": 4,
+  "04000US32": 8,
+  "04000US33": 1,
+  "04000US34": 2,
+  "04000US35": 8,
+  "04000US36": 2,
+  "04000US37": 5,
+  "04000US38": 4,
+  "04000US39": 3,
+  "04000US40": 7,
+  "04000US41": 9,
+  "04000US42": 2,
+  "04000US44": 1,
+  "04000US45": 5,
+  "04000US46": 4,
+  "04000US47": 6,
+  "04000US48": 7,
+  "04000US49": 8,
+  "04000US50": 1,
+  "04000US51": 5,
+  "04000US53": 9,
+  "04000US54": 5,
+  "04000US55": 3,
+  "04000US56": 8
+};
+
+export const divisions = [
+  {
+    "ID Region": 1,
+    "Region": "Northeast",
+    "ID Division": 1,
+    "Division": "New England"
+  },
+  {
+    "ID Region": 1,
+    "Region": "Northeast",
+    "ID Division": 2,
+    "Division": "Middle Atlantic"
+  },
+  {
+    "ID Region": 2,
+    "Region": "Midwest",
+    "ID Division": 3,
+    "Division": "East North Central"
+  },
+  {
+    "ID Region": 2,
+    "Region": "Midwest",
+    "ID Division": 4,
+    "Division": "West North Central"
+  },
+  {
+    "ID Region": 3,
+    "Region": "South",
+    "ID Division": 5,
+    "Division": "South Atlantic"
+  },
+  {
+    "ID Region": 3,
+    "Region": "South",
+    "ID Division": 6,
+    "Division": "East South Central"
+  },
+  {
+    "ID Region": 3,
+    "Region": "South",
+    "ID Division": 7,
+    "Division": "West South Central"
+  },
+  {
+    "ID Region": 4,
+    "Region": "West",
+    "ID Division": 8,
+    "Division": "Mountain"
+  },
+  {
+    "ID Region": 4,
+    "Region": "West",
+    "ID Division": 9,
+    "Division": "Pacific"
+  },
+  {
+    "ID Region": 5,
+    "Region": "Other",
+    "ID Division": 10,
+    "Division": "Other"
+  }
+];
