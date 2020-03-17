312dc9ec3b70cf0e5fea703289a1e294b877d01b
Dave Landry <landry.dave@gmail.com>
2020-03-17 18:59:41 -0400

    round 2 of coronavirus page

--- app/pages/Coronavirus.jsx
+++ app/pages/Coronavirus.jsx
@@ -4,13 +4,15 @@ import axios from "axios";
 import {NonIdealState, Spinner} from "@blueprintjs/core";
 import {Helmet} from "react-helmet";
 
-import {extent, max, merge, range} from "d3-array";
+import {max, merge} from "d3-array";
 import {nest} from "d3-collection";
 import {timeFormat} from "d3-time-format";
 const dayFormat = timeFormat("%A %B %d");
+const dateFormat = timeFormat("%b %d");
 const hourFormat = timeFormat("%I:%M %p");
 
 import {colorLegible} from "d3plus-color";
+import {assign, unique} from "d3plus-common";
 import {formatAbbreviate} from "d3plus-format";
 import {titleCase} from "d3plus-text";
 import {LinePlot} from "d3plus-react";
@@ -26,10 +28,12 @@ class Coronavirus extends Component {
     super(props);
     this.state = {
       cutoff: 10,
+      countries: false,
       data: false,
       date: false,
       level: "state",
       measure: "Confirmed",
+      scale: "linear",
       title: `Coronavirus Outbreak by ${titleCase("state")}`
     };
   }
@@ -41,19 +45,31 @@ class Coronavirus extends Component {
   componentDidMount() {
     const {title} = this.state;
     this.props.updateTitle(title);
-    axios.get("/api/coronavirus/all")
+    axios.get("/api/coronavirus/state")
       .then(resp => resp.data)
       .then(resp => {
-        const data = resp.data.map(d => {
-          if (d.Level === "state") {
-            const dID = stateToDivision[d["ID Geography"]];
-            let division = divisions.find(x => x["ID Division"] === dID);
-            if (!division) division = divisions.find(x => x["ID Division"] === 5);
-            return Object.assign(d, division);
-          }
-          return d;
-        });
-        this.setState({data, date: new Date(resp.timestamp)});
+
+        const data = resp.data
+          .map(d => {
+            d.Date = new Date(d.Date).getTime();
+            d.ConfirmedPC = d.Confirmed / resp.population[d["ID Geography"]] * 100000;
+            if (d.Level === "state") {
+              const dID = stateToDivision[d["ID Geography"]];
+              let division = divisions.find(x => x["ID Division"] === dID);
+              if (!division) division = divisions.find(x => x["ID Division"] === 5);
+              return Object.assign(d, division);
+            }
+            return d;
+          });
+
+        const countries = resp.countries
+          .map(d => {
+            d.Date = new Date(d.Date).getTime();
+            d.ConfirmedPC = d.Confirmed / resp.world[d.Geography] * 100000;
+            return d;
+          });
+
+        this.setState({countries, data, date: new Date(resp.timestamp)});
       })
       .catch(() => this.setState({data: "Error loading data, please try again later."}));
   }
@@ -62,12 +78,15 @@ class Coronavirus extends Component {
     this.setState({cutoff: +event.target.value});
   }
 
+  changeScale(event) {
+    this.setState({scale: event.target.value});
+  }
+
   render() {
 
-    const {cutoff, data, date, level, measure, title} = this.state;
+    const {countries, cutoff, data, date, level, measure, scale, title} = this.state;
 
-    const h = typeof window !== "undefined" ? window.innerHeight : 800,
-          w = typeof window !== "undefined" ? window.innerWidth : 1200;
+    const w = typeof window !== "undefined" ? window.innerWidth : 1200;
 
     const filteredData = data instanceof Array ? data.filter(d => d.Level === level) : [];
 
@@ -77,13 +96,8 @@ class Coronavirus extends Component {
       .map(group => {
         let days = 0;
         return group.values
-          .reduce((arr, d, i) => {
+          .reduce((arr, d) => {
             if (d.Confirmed >= cutoff) {
-              if (days === 0) {
-                const starter = group.values[i - 1];
-                starter.Days = days;
-                arr.push(starter);
-              }
               days++;
               d.Days = days;
               arr.push(d);
@@ -93,9 +107,12 @@ class Coronavirus extends Component {
       }).sort((a, b) => max(b, d => d[measure]) - max(a, d => d[measure])));
 
     const maxValue = max(vizData, d => d[measure]);
-    const ratio = h / maxValue;
+    const maxValuePC = max(vizData, d => d[`${measure}PC`]);
+    const maxValueWorldPC = max(countries, d => d[`${measure}PC`]);
+    const minValue = scale === "log" ? 1 : 0;
 
-    const labelOffsets = {};
+    const showLabel = {};
+    const labelSpace = 5;
     nest()
       .key(d => d["ID Geography"])
       .entries(vizData)
@@ -105,20 +122,108 @@ class Coronavirus extends Component {
           .filter(v => v.Days === d.Days)
           .sort((a, b) => a[measure] - b[measure]);
         const i = compares.indexOf(d);
-        const mod = Math.log10(d[measure]) / 0.5;
         const prevDiff = compares[i - 1] ? d[measure] - compares[i - 1][measure] : Infinity;
         const nextDiff = compares[i + 1] ? compares[i + 1][measure] - d[measure] : Infinity;
-        if (prevDiff * ratio > mod && nextDiff * ratio > mod) {
-          labelOffsets[group.key] = 0;
+        if (prevDiff > labelSpace || nextDiff > labelSpace) {
+          showLabel[group.key] = true;
         }
       });
 
-    let xDomain = [];
+    let daysDomain = [];
     if (vizData.length) {
-      const xExtent = extent(vizData, d => d.Days);
-      xDomain = range(xExtent[0], xExtent[1] + 2, 1);
+      daysDomain = unique(vizData.map(d => d.Days)).sort();
+      const lastDays = daysDomain[daysDomain.length - 1];
+      daysDomain.push(lastDays + 1);
     }
-    const labelWidth = xDomain.length ? w / (xDomain[xDomain.length - 1] + 1) : 0;
+
+    let dateDomain = [];
+    if (vizData.length) {
+      dateDomain = unique(vizData.map(d => d.Date)).sort();
+      const lastDate = new Date(dateDomain[dateDomain.length - 1]);
+      const nextDate = lastDate.setDate(lastDate.getDate() + 1);
+      dateDomain.push(nextDate);
+    }
+
+    let worldDomain = [];
+    if (vizData.length) {
+      worldDomain = unique(countries.map(d => d.Date)).sort();
+      let lastDate = worldDomain[worldDomain.length - 1];
+      const endDate = lastDate + (lastDate  - worldDomain[0]) / 15;
+      lastDate = new Date(lastDate);
+      while (worldDomain[worldDomain.length - 1] < endDate) {
+        lastDate.setDate(lastDate.getDate() + 1);
+        worldDomain.push(lastDate.getTime());
+      }
+    }
+
+    const labelWidth = daysDomain.length ? w / (daysDomain[daysDomain.length - 1] + 1) : 0;
+
+    const sharedConfig = {
+      aggs: {
+        "ID Division": arr => arr[0],
+        "ID Region": arr => arr[0]
+      },
+      data: vizData,
+      discrete: "x",
+      groupBy: ["ID Region", "ID Geography"],
+      label: d => d.Geography instanceof Array ? d.Region : d.Geography,
+      legendTooltip: {
+        tbody: []
+      },
+      on: {
+        mouseenter(d) {
+          if (d["ID Geography"] instanceof Array) this.hover(h => (h.data || h)["ID Region"] === d["ID Region"]);
+          else this.hover(h => h["ID Geography"] === d["ID Geography"]);
+        }
+      },
+      shapeConfig: {
+        hoverOpacity: 0.25,
+        Line: {
+          label: d => d.Geography,
+          labelConfig: {
+            fontColor: d => colorLegible(colors.Region[d["ID Region"]]),
+            fontFamily: () => ["Pathway Gothic One", "Arial Narrow", "sans-serif"],
+            fontSize: () => 14,
+            padding: 0,
+            verticalAlign: "middle"
+          },
+          labelBounds: (d, i, s) => {
+            if (showLabel[d["ID Geography"]]) {
+              const [firstX, firstY] = s.points[0];
+              const [lastX, lastY] = s.points[s.points.length - 1];
+              const height = 30;
+              return   {
+                x: lastX - firstX + 5,
+                y: lastY - firstY - height / 2 + 1,
+                width: labelWidth,
+                height
+              };
+            }
+            return false;
+          },
+          stroke: d => colors.Region[d["ID Region"]]
+        }
+      },
+      tooltipConfig: {
+        tbody: [
+          ["Date", d => dayFormat(new Date(d.Date))],
+          ["Confirmed", d => formatAbbreviate(d.Confirmed)],
+          ["Cases per 100,000", d => formatAbbreviate(d.ConfirmedPC)],
+          ["Recovered", d => formatAbbreviate(d.Recovered)],
+          ["Deaths", d => formatAbbreviate(d.Deaths)]
+        ]
+      },
+      y: measure,
+      yConfig: {
+        barConfig: {
+          stroke: "transparent"
+        },
+        domain: [minValue, maxValue],
+        scale,
+        tickFormat: formatAbbreviate,
+        title: `${measure}${measure !== "Deaths" ? " Cases" : ""} (${scale === "log" ? "Logarithmic" : "Linear"})`
+      }
+    };
 
     return <div id="Coronavirus">
 
@@ -126,43 +231,79 @@ class Coronavirus extends Component {
         <meta property="og:title" content={ `${title} | Data USA` } />
       </Helmet>
 
-      <div className="coronavirus-cutoff">Days Since <select value={cutoff} onChange={this.changeCutoff.bind(this)}>
-        <option value="1">1</option>
-        <option value="10">10</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
-      </select> Confirmed Cases</div>
-
       <p className="coronavirus-source">Data is sourced from <a href="https://github.com/CSSEGISandData/COVID-19" target="_blank" rel="noopener noreferrer">Johns Hopkins CSSE</a>.</p>
       { date && <p className="coronavirus-updated-at">Last updated on {dayFormat(date)} at {hourFormat(date)}.</p> }
 
-      <div id="coronavirus-main-viz">
-        { data && data instanceof Array
-          ? <LinePlot config={{
-            aggs: {
-              "ID Division": arr => arr[0],
-              "ID Region": arr => arr[0]
-            },
-            data: vizData,
-            discrete: "x",
-            groupBy: ["ID Region", "ID Geography"],
-            label: d => d.Geography instanceof Array ? d.Region : d.Geography,
-            legendTooltip: {
-              tbody: []
-            },
-            shapeConfig: {
-              Line: {
-                label: d => d.Geography,
-                labelConfig: {
-                  fontColor: d => colorLegible(colors.Region[d["ID Region"]]),
-                  fontFamily: () => ["Pathway Gothic One", "Arial Narrow", "sans-serif"],
-                  fontSize: () => 14,
-                  padding: 0,
-                  verticalAlign: "middle"
-                },
-                labelBounds: (d, i, s) => {
-                  const offset = labelOffsets[d["ID Geography"]];
-                  if (offset !== undefined) {
+      { data && data instanceof Array
+        ? <div id="coronavirus-main">
+          <div className="coronavirus-viz">
+            <h2>Confirmed Cases Over Time</h2>
+            <p>Y-Axis Scale <select value={scale} onChange={this.changeScale.bind(this)}>
+              <option value="linear">Linear</option>
+              <option value="log">Logarithmic</option>
+            </select></p>
+            <LinePlot config={assign({}, sharedConfig, {
+              x: "Date",
+              xConfig: {
+                domain: dateDomain,
+                tickFormat: dateFormat
+              }
+            })} />
+          </div>
+          <div className="coronavirus-viz">
+            <h2>Days Since <select value={cutoff} onChange={this.changeCutoff.bind(this)}>
+              <option value="1">1</option>
+              <option value="10">10</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select> Confirmed Cases</h2>
+            <p>Y-Axis Scale <select value={scale} onChange={this.changeScale.bind(this)}>
+              <option value="linear">Linear</option>
+              <option value="log">Logarithmic</option>
+            </select></p>
+            <LinePlot config={assign({}, sharedConfig, {
+              x: "Days",
+              xConfig: {
+                domain: daysDomain,
+                tickFormat: d => `${formatAbbreviate(d)} day${d !== 1 ? "s" : ""}`,
+                title: `Days Since ${cutoff} Confirmed Cases`
+              }
+            })} />
+          </div>
+          <div className="coronavirus-viz">
+            <h2>Confirmed Cases per 100,000 Population</h2>
+            <LinePlot config={assign({}, sharedConfig, {
+              x: "Date",
+              xConfig: {
+                domain: dateDomain,
+                tickFormat: dateFormat
+              },
+              y: `${measure}PC`,
+              yConfig: {
+                domain: [0, maxValuePC],
+                scale: "linear",
+                title: `${measure}${measure !== "Deaths" ? " Cases" : ""} per 100,000`
+              }
+            })} />
+          </div>
+          <div className="coronavirus-viz">
+            <h2>United States Compared to Most Highly Effected Countries</h2>
+            <LinePlot config={assign({}, sharedConfig, {
+              data: countries,
+              groupBy: "Geography",
+              label: d => d.Geography instanceof Array ? "International" : d.Geography,
+              on: {
+                mouseenter(d) {
+                  if (d.Geography instanceof Array) this.hover(h => (h.data || h).Geography === d.Geography);
+                  else this.hover(h => h.Geography === d.Geography);
+                }
+              },
+              shapeConfig: {
+                Line: {
+                  labelConfig: {
+                    fontColor: d => colorLegible(d.Geography === "United States" ? colors.colorHighlight : colors.colorGrey)
+                  },
+                  labelBounds: (d, i, s) => {
                     const [firstX, firstY] = s.points[0];
                     const [lastX, lastY] = s.points[s.points.length - 1];
                     const height = 30;
@@ -172,40 +313,28 @@ class Coronavirus extends Component {
                       width: labelWidth,
                       height
                     };
-                  }
-                  return false;
+                  },
+                  stroke: d => d.Geography === "United States" ? colors.colorHighlight : colors.colorGrey
                 },
-                stroke: d => colors.Region[d["ID Region"]]
-              }
-            },
-            tooltipConfig: {
-              tbody: [
-                ["Date", d => dayFormat(new Date(d.Date))],
-                ["Confirmed", d => formatAbbreviate(d.Confirmed)],
-                ["Recovered", d => formatAbbreviate(d.Recovered)],
-                ["Deaths", d => formatAbbreviate(d.Deaths)]
-              ]
-            },
-            x: "Days",
-            xConfig: {
-              domain: xDomain,
-              tickFormat: d => `${formatAbbreviate(d)} day${d !== 1 ? "s" : ""}`,
-              title: `Days Since ${cutoff} Confirmed Cases`
-            },
-            y: measure,
-            yConfig: {
-              barConfig: {
-                stroke: "transparent"
+                sort: a => a.Geography === "United States" ? 1 : -1
               },
-              domain: [1, maxValue],
-              scale: "log",
-              tickFormat: formatAbbreviate,
-              title: `${measure}${measure !== "Deaths" ? " Cases" : ""} (log)`
-            }
-          }} />
-          : typeof data === "string" ? <NonIdealState title={data} />
-            : <NonIdealState title="Loading Data..." icon={<Spinner />} /> }
-      </div>
+              x: "Date",
+              xConfig: {
+                domain: worldDomain,
+                tickFormat: dateFormat
+              },
+              y: `${measure}PC`,
+              yConfig: {
+                domain: [0, maxValueWorldPC],
+                scale: "linear",
+                title: `${measure}${measure !== "Deaths" ? " Cases" : ""} per 100,000`
+              }
+            })} />
+          </div>
+        </div>
+        : typeof data === "string" ? <NonIdealState title={data} visual="error" />
+          : <NonIdealState title="Loading Data..." visual={<Spinner />} /> }
+
     </div>;
 
   }
