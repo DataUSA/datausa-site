851651a08f65392bf85d3c1b28da2f29797abfa2
Dave Landry <landry.dave@gmail.com>
2020-12-08 18:20:20 -0500

    updates Viz Builder example links

--- app/pages/VisualizeExamples.js
+++ app/pages/VisualizeExamples.js
@@ -15,31 +15,31 @@ export const examples = [
     title: "Foreign-Born Citizens by State",
     group: "Heritage & Demographics",
     image: "/api/profile/cip/regional-studies-us-canadian-foreign/thumb",
-    link: "/visualize?groups=0-TBhjH&measure=64auG"
+    link: "/visualize?measure=Z1qIz4e&groups%5B0%5D=Z2pfmiM%7C0"
   },
   {
     title: "Russian Speakers by State",
     group: "Heritage & Demographics",
     image: "/images/profile/russia.jpg",
-    link: "/visualize?groups=0-yiXGF&groups=1-1ac173-9&measure=ZJtXqi"
+    link: "/visualize?measure=Z1yRSMb&groups%5B0%5D=Z2aE2zE%7C0&groups%5B1%5D=Z10CxXn%7C0%7C9"
   },
   {
     title: "Household Income by Race",
     group: "Heritage & Demographics",
     image: "/api/profile/cip/public-finance/thumb",
-    link: "/visualize?groups=0-Z169Wxp&measure=Zn7xp5"
+    link: "/visualize?measure=1tGN53&groups%5B0%5D=14J23o%7C0"
   },
   {
     title: "Poverty Rate by Race",
     group: "Heritage & Demographics",
     image: "/api/profile/cip/demography-population-studies-6/thumb",
-    link: "/visualize?groups=0-28NmKu&measure=AFRAP"
+    link: "/visualize?measure=2cLxkH&groups%5B0%5D=Z1z6JHU%7C0"
   },
   {
     title: "Average Age by State",
     group: "Heritage & Demographics",
     image: "/api/profile/soc/childcare-workers/thumb",
-    link: "/visualize?groups=0-z9TnC&measure=1Kkgfr"
+    link: "/visualize?measure=Z1NPUMo&groups%5B0%5D=Z19J7ox%7C0"
   },
 
   /* Health */
@@ -48,46 +48,46 @@ export const examples = [
     title: "Opioid Deaths by County",
     group: "Health",
     image: "/api/profile/cip/pharmacology/thumb",
-    link: "/visualize?groups=0-QuaqK&measure=2sUCF4"
+    link: "/visualize?measure=Z2kiFFG&groups%5B0%5D=Z2dgJDo%7C0"
   },
   {
     title: "Adult Obesity by County",
     group: "Health",
     image: "/api/profile/cip/190505/thumb",
-    link: "/visualize?groups=0-Z1X72Pg&measure=Z1iORxu"
+    link: "/visualize?measure=ELcXt&groups%5B0%5D=2tql3o%7C0"
   },
   {
     title: "Food Environment Index",
     group: "Health",
     image: "/api/profile/cip/120507/thumb",
-    link: "/visualize?groups=0-Z1X72Pg&measure=Z1xNbvc"
+    link: "/visualize?measure=1tYafz&groups%5B0%5D=2tql3o%7C0"
   },
   {
     title: "Adults with Major Depressive Disorder by State",
     group: "Health",
     image: "/api/profile/cip/420101/thumb",
-    link: "/visualize?groups=0-Zc58ag&measure=3DN30"
+    link: "/visualize?measure=Z1y70h&groups%5B0%5D=13bP73%7C0"
   },
   {
     title: "Adults who Cannot Afford a Doctor",
     group: "Health",
     image: "/api/profile/naics/6211/thumb",
-    link: "/visualize?groups=0-14F1wI&measure=1wObYo"
+    link: "/visualize?measure=hGlgy&groups%5B0%5D=Z22AffI%7C0"
   },
 
   /* Housing */
 
   {
-    title: "Home Ownership by State",
+    title: "Homeownership by State",
     group: "Housing",
     image: "/api/profile/naics/531/thumb",
-    link: "/visualize?groups=0-Z4LzeD&measure=Z1X8D4i"
+    link: "/visualize?measure=qdB4t&groups%5B0%5D=Z1OYOhe%7C0"
   },
   {
     title: "Average Commute Time by Metro Area",
     group: "Housing",
     image: "/api/profile/soc/474051/thumb",
-    link: "/visualize?groups=0-ZNTHUM&measure=ZMlbux"
+    link: "/visualize?measure=Z1Qneyq&groups%5B0%5D=Z2dDEPP%7C0"
   },
   {
     title: "People Driving Alone to Work by State",
@@ -99,46 +99,46 @@ export const examples = [
     title: "Renter Occupied Households by State",
     group: "Housing",
     image: "/api/profile/naics/4232/thumb",
-    link: "/visualize?groups=0-Z4LzeD&groups=1-Z1xMhVU-1&measure=Z1X8D4i"
+    link: "/visualize?measure=qdB4t&groups%5B0%5D=Z1OYOhe%7C0&groups%5B1%5D=1eGFSs%7C0%7C1"
   },
   {
-    title: "Households with Internet Access by State",
+    title: "Households with No Internet Access by State",
     group: "Housing",
     image: "/api/profile/cip/computer-science-6/thumb",
-    link: "/visualize?groups=0-2x4fRF&groups=1-ZscJU2-0~1&measure=Z1CV1rs"
+    link: "/visualize?measure=2i0agU&groups%5B0%5D=Zi1xWJ%7C0&groups%5B1%5D=ZXbkwr%7C0%7C2"
   },
 
   /* Labor */
 
   {
-    title: "Black Females working in the Software Industry by State",
+    title: "Black Females working in the Software Industry",
     group: "Labor",
     image: "/api/profile/soc/151131/thumb",
-    link: "/visualize?groups=0-z9TnC&groups=1-Z1Oby8M-2&groups=2-1mjmRl-5112&groups=3-1dQe8s-2&measure=1qWfo"
+    link: "/visualize?measure=226bN8&groups%5B0%5D=zo2S4%7C0%7C2&groups%5B1%5D=WmgLA%7C0%7C2&groups%5B2%5D=Z1GaN5f%7C0%7C5112&time=2017"
   },
   {
     title: "German-Borns Working in the Performing Arts Industry",
     group: "Labor",
     image: "/api/profile/cip/509999/thumb",
-    link: "/visualize?groups=0-z9TnC&groups=1-1SyFhe-110&groups=2-1mjmRl-711&measure=1qWfo"
+    link: "/visualize?measure=226bN8&groups%5B0%5D=Z12ujhd%7C0%7C110&groups%5B1%5D=Z1GaN5f%7C0%7C711&time=2017"
   },
   {
     title: "Naturalized US Citizens Working as Computer Scientists and Web Developers",
     group: "Labor",
     image: "/api/profile/cip/general-computer-programming/thumb",
-    link: "/visualize?groups=0-13xVLW-4&groups=1-19hV1x-151111~151131~151134~15113X&measure=1qWfo"
+    link: "/visualize?measure=226bN8&groups%5B0%5D=1DKHyo%7C0%7C4&groups%5B1%5D=Z1tziHm%7C0%7C151111%7C151134&time=2017"
   },
   {
     title: "Median Earnings in the Construction Industry by State",
     group: "Labor",
     image: "/api/profile/cip/46/thumb",
-    link: "/visualize?groups=0-Z1DVCsC&groups=1-Z1vDbOy-2&measure=ALgX7"
+    link: "/visualize?measure=mpiHx&groups%5B0%5D=ZbimRG%7C0&groups%5B1%5D=Z13YGbt%7C0%7C4&time=2018"
   },
   {
     title: "Income Inequality by Metro Area",
     group: "Labor",
     image: "/api/profile/napcs/41/thumb",
-    link: "/visualize?groups=0-23eSQ7&measure=230vWl"
+    link: "/visualize?measure=Z1WmeWG&groups%5B0%5D=Z1vlgV9%7C0&time=2018"
   },
   // {
   //   title: "Coal Mining Workers by State",
@@ -153,31 +153,31 @@ export const examples = [
     title: "Admissions for Universities in the Boston Metro Area",
     group: "Education",
     image: "/api/profile/geo/boston-cambridge-quincy-ma-nh-metro-area/thumb",
-    link: "/visualize?groups=0-1CdfJW&groups=1-2clFcR-31000US14460&measure=Z1GSog1"
+    link: "/visualize?measure=Z1rjtkk&groups%5B0%5D=Z2tV9Ui%7C0&groups%5B1%5D=ZuCNTF%7C0%7C31000US14460&time=2017"
   },
   {
     title: "Default Rate by State",
     group: "Education",
     image: "/api/profile/soc/434131/thumb",
-    link: "/visualize?groups=0-24s1dP&measure=ZdVPNx"
+    link: "/visualize?measure=UULi4&groups%5B0%5D=4Pksr%7C0&time=2016"
   },
   {
     title: "Engineering Graduates by Gender",
     group: "Education",
     image: "/api/profile/cip/14/thumb",
-    link: "/visualize?groups=0-BNDBG&groups=1-Z2i3TId-14&measure=1g4zN3"
+    link: "/visualize?measure=E4OdG&groups%5B0%5D=Z12O6Gw%7C0&groups%5B1%5D=1BPORw%7C0%7C14&time=2017"
   },
   {
     title: "Average Number of Off-Campus Students by State",
     group: "Education",
     image: "/api/profile/cip/railroad-railway-transportation/thumb",
-    link: "/visualize?groups=0-JrDjD-Off+campus&groups=1-1kUYYy&measure=Z7tI0u"
+    link: "/visualize?measure=Z2hQkn7&groups%5B0%5D=Z1nPfqV%7C0&groups%5B1%5D=2qzn0E%7C0%7COff+campus&time=2017"
   },
   {
     title: "Benefit Expenditure by University Group",
     group: "Education",
     image: "/api/profile/soc/healthcare-support-workers-all-other-including-medical-equipment-preparers/thumb",
-    link: "/visualize?groups=0-1XM2Wg&measure=zY3EL"
+    link: "/visualize?measure=17ePnX&groups%5B0%5D=Z3oGKd%7C0&time=2015"
   },
 
   /* Government */
@@ -186,30 +186,30 @@ export const examples = [
     title: "Department of Defense Spending by State",
     group: "Government",
     image: "/api/profile/soc/550000/thumb",
-    link: "/visualize?groups=0-Z1MxM8L&groups=1-1pz0Cl-97&measure=1e64mv"
+    link: "/visualize?measure=Z1LxWvG&groups%5B0%5D=Z1tpPKe%7C0&time=2017"
   },
   {
     title: "National Science Foundation Grants by State",
     group: "Government",
     image: "/api/profile/soc/1930XX/thumb",
-    link: "/visualize?groups=0-Z1MxM8L&groups=1-1pz0Cl-49&groups=2-Z27NDkh-Grant&measure=1e64mv"
+    link: "/visualize?measure=Z1LxWvG&groups%5B0%5D=Z1tpPKe%7C0&groups%5B1%5D=2pvOai%7C0%7C4900&groups%5B2%5D=ZjWAq6%7C0%7CGrant&time=2017"
   },
   {
     title: "Department of Interior Spending by State",
     group: "Government",
     image: "/api/profile/geo/washington-dc/thumb",
-    link: "/visualize?groups=0-Z1MxM8L&groups=1-1pz0Cl-14&measure=1e64mv"
+    link: "/visualize?measure=Z1LxWvG&groups%5B0%5D=Z1tpPKe%7C0&groups%5B1%5D=2pvOai%7C0%7C1400&time=2017"
   },
   {
     title: "World War II Veterans by State",
     group: "Government",
     image: "/api/profile/naics/active-duty-military/thumb",
-    link: "/visualize?groups=0-16Tk7O&groups=1-Z226eaa-4&measure=Z1LTgi2"
+    link: "/visualize?measure=QXkox&groups%5B0%5D=2nkSWt%7C0&groups%5B1%5D=1nxNQl%7C0%7C4&time=2018"
   },
   {
     title: "Public Administration Workforce by Race",
     group: "Government",
     image: "/api/profile/naics/public-administration/thumb",
-    link: "/visualize?groups=0-23ay2s-92&groups=1-1dQe8s&measure=1qWfo"
+    link: "/visualize?measure=226bN8&groups%5B0%5D=Z1bzp01%7C0%7C92&groups%5B1%5D=zo2S4%7C0&time=2017"
   }
 ];
