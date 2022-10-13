45a7283fbe0b300004f60b899d76330db3d5e018
Dave Landry <landry.dave@gmail.com>
2022-10-13 16:47:09 -0400

    replaces Profile SubNav and SideNav with the canon-cms Subnav component

--- app/profile/index.jsx
+++ app/profile/index.jsx
@@ -3,15 +3,12 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {Helmet} from "react-helmet-async";
 
-import {AnchorLink, fetchData} from "@datawheel/canon-core";
-import SubNav from "toCanon/SubNav";
+import {fetchData} from "@datawheel/canon-core";
+import Subnav from "@datawheel/canon-cms/src/components/sections/components/Subnav";
 import Splash from "toCanon/Splash";
-import SectionIcon from "toCanon/SectionIcon";
 import Topic from "toCanon/Topic";
 
-import {Position, Tooltip} from "@blueprintjs/core";
 import axios from "axios";
-import {select} from "d3-selection";
 import "./index.css";
 
 import Loading from "components/Loading";
@@ -21,77 +18,17 @@ import {updateTitle} from "actions/title";
 
 import NotFound from "../pages/NotFound/NotFound";
 
-const categories = {
-  geo: [
-    {title: "Wages", slug: "wages", topic: "income", section: "economy"},
-    {title: "Occupations", slug: "occupations", topic: "tmap_occ_num_emp", section: "economy"},
-    {title: "Industries", slug: "industries", topic: "tmap_ind_num_emp", section: "economy"},
-    {title: "Domestic Trade", slug: "trade", topic: "domestic_trade", section: "economy"},
-    {title: "Race and Ethnicity", slug: "race-and-ethnicity", topic: "ethnicity", section: "demographics"},
-    {title: "Heritage", slug: "heritage", topic: "foreign_born", section: "demographics"},
-    {title: "Military", slug: "military", topic: "veterans", section: "demographics"},
-    {title: "Housing", slug: "housing", topic: "property_value", section: "housing"},
-    // {title: "Income", slug: "income", topic: "household_income", section: "housing"},
-    {title: "Transportation", slug: "transportation", topic: "num_vehicles", section: "housing"}
-  ],
-  university: [
-    {title: "Student Expenses", slug: "student_expenses", topic: "tuition", section: "costs"},
-    {title: "Financial Aid", slug: "financial_aid", topic: "incomeawards", section: "costs"},
-    {title: "Workforce", slug: "workforce", topic: "jobs", section: "graduates"},
-    {title: "Degrees", slug: "degrees", topic: "majors", section: "graduates"},
-    {title: "Diversity", slug: "diversity", topic: "rate", section: "graduates"},
-    {title: "Finances", slug: "finances", topic: "endowment", section: "operations"},
-    {title: "Expenses", slug: "expenses", topic: "salaries", section: "operations"},
-    {title: "Faculty and Staff", slug: "faculty_and_staff", topic: "staff_tmap", section: "operations"}
-  ],
-  naics: [
-    {title: "Occupations", slug: "occupations", topic: "tmap_top_occs", section: "workforce"},
-    {title: "Wages", slug: "wages", topic: "wages", section: "workforce"},
-    {title: "Opportunities", slug: "opportunities", topic: "wage_geo_rca", section: "workforce"}
-  ],
-  soc: [
-    {title: "Wages", slug: "wages", topic: "wage_by_industry", section: "employment"},
-    {title: "Industries", slug: "industries", topic: "top_ind_num_emp", section: "employment"}
-  ],
-  cip: [
-    {title: "Wages", slug: "wages", topic: "top_income", section: "employment"},
-    {title: "Occupations", slug: "occupations", topic: "top_occ_num_emp", section: "employment"},
-    {title: "Industries", slug: "industries", topic: "top_ind_num_emp", section: "employment"}
-  ]
-};
-
 class Profile extends Component {
 
   constructor(props) {
 
     super(props);
 
-    const {slug} = props.params;
-
-    const cats = categories[slug] || [];
-    const sidenav = [];
-    const sections = (!props.profile.error ? props.profile.sections : []);
-
-    sections
-      .slice(2)
-      .filter(d => d.type === "Grouping")
-      .forEach(s => {
-        const sectionCats = cats
-          .filter(c => c.section === s.slug && sections.find(ss => ss.slug === c.topic));
-        if (sectionCats.length) sidenav.push(sectionCats.map(s => ({title: s.title, slug: `category_${s.slug}`})));
-        else sidenav.push([{title: s.title.replace(/<[^>]+>/g, ""), slug: s.slug}]);
-      });
-
     this.state = {
-      activeSection: false,
-      activeSidenav: false,
       comparisons: [],
       loading: false,
-      profile: props.profile,
-      showSidenav: false,
-      sidenav
+      profile: props.profile
     };
-    this.scrollBind = this.handleScroll.bind(this);
   }
 
   getChildContext() {
@@ -153,8 +90,6 @@ class Profile extends Component {
       const title = stripHTML(profiles.map(d => d.title).join(" & "));
       this.props.updateTitle(title);
 
-      window.addEventListener("scroll", this.scrollBind);
-      this.scrollBind();
       const {query} = this.props.location;
       if (query.compare) this.addComparison.bind(this)(query.compare);
 
@@ -164,49 +99,6 @@ class Profile extends Component {
 
   componentWillUnmount() {
     this.props.updateTitle(false);
-    if (!this.state.profile.error) {
-      window.removeEventListener("scroll", this.scrollBind);
-    }
-  }
-
-  handleScroll() {
-
-    const sections = this.state.profile.sections
-      .slice(2)
-      .filter(d => d.type === "Grouping");
-
-    const {activeSection, activeSidenav, showSidenav, sidenav} = this.state;
-    const navHeight = 85;
-
-    let newActiveSection = false;
-    const elem = document.getElementById("about");
-    const top = elem ? elem.getBoundingClientRect().top : 1;
-    if (top <= navHeight) newActiveSection = "about";
-    sections.forEach(section => {
-      const elem = document.getElementById(section.slug);
-      const top = elem ? elem.getBoundingClientRect().top : 1;
-      if (top <= navHeight) newActiveSection = section.slug;
-    });
-
-    let newActiveSidenav = false;
-    sidenav.forEach(section => {
-      section.forEach(category => {
-        const elem = document.getElementById(category.slug);
-        const top = elem ? elem.getBoundingClientRect().top : 1;
-        if (top <= navHeight) newActiveSidenav = category.slug;
-      });
-    });
-
-    const newShowSidenav = newActiveSection && newActiveSection !== "about" && document.getElementById("keep-exploring").getBoundingClientRect().top > window.innerHeight;
-
-    if (activeSection !== newActiveSection || activeSidenav !== newActiveSidenav || showSidenav !== newShowSidenav) {
-      this.setState({
-        activeSection: newActiveSection,
-        activeSidenav: newActiveSidenav,
-        showSidenav: newShowSidenav
-      });
-    }
-
   }
 
   onSelector(name, value, callback) {
@@ -234,7 +126,7 @@ class Profile extends Component {
 
     const {stripHTML} = this.context.formatters;
     const {slug} = params;
-    const {activeSection, activeSidenav, comparisons, loading, showSidenav, sidenav} = this.state;
+    const {comparisons, loading} = this.state;
     const joiner = ["geo"].includes(slug) ? "in" : "for";
 
     const profiles = [profile].concat(comparisons);
@@ -245,7 +137,6 @@ class Profile extends Component {
     const GroupingSections = profile.sections.filter(d => d.type === "Grouping");
     const GroupingIndices = GroupingSections.map(s => profile.sections.indexOf(s));
     const topics = [];
-    const cats = categories[slug] || [];
 
     GroupingSections
       .forEach((s, i) => {
@@ -254,14 +145,6 @@ class Profile extends Component {
         profile.sections
           .slice(GroupingIndices[i] + 1, GroupingIndices[i + 1])
           .forEach(t => {
-            const cat = cats.find(c => c.topic === t.slug);
-            if (cat) {
-              arr.push([
-                <h2 id={`category_${cat.slug}`} className="category" key={`category_${cat.slug}`}>
-                  <AnchorLink to={`category_${cat.slug}`}>{cat.title}</AnchorLink>
-                </h2>
-              ]);
-            }
             t.section = s.slug;
             const sectionCompares = comparisons.map(c => c.sections.find(ss => ss.id === t.id)).filter(Boolean);
             if (comparisons.length) t.titleCompare = t.title.replace("</p>", ` ${joiner} ${stripHTML(profile.title)}</p>`);
@@ -279,6 +162,56 @@ class Profile extends Component {
     const aboutSection = profile.sections.find(s => s.slug === "about");
     const metaDesc = aboutSection.descriptions.length ? stripHTML(aboutSection.descriptions[0].description) : false;
 
+    /**
+     * The following `groupedSections` logic was taken from the @datawheel/canon-cms
+     * ProfileRenderer in order to provide the correct data shape to the SubNav component.
+     */
+    let {sections} = profile;
+    // Find the first instance of a Hero section (excludes all following instances)
+    const heroSection = sections.find(l => l.type === "Hero");
+    // Remove all heros & modals from sections.
+    if (heroSection) sections = sections.filter(l => l.type !== "Hero");
+    sections = sections.filter(l => l.position !== "modal");
+
+    // rename old section names
+    sections.forEach(l => {
+      if (!l.slug) l.slug = `section-${l.id}`;
+    });
+
+    let groupedSections = [];
+
+    // make sure there are sections to loop through (issue #700)
+    if (sections.length) {
+
+      const groupableSections = ["SingleColumn", "Column"]; // sections to be grouped together
+      const innerGroupedSections = []; // array for sections to be accumulated into
+      // reduce sections into a nested array of groupedSections
+      innerGroupedSections.push(sections.reduce((arr, section) => {
+        if (arr.length === 0) arr.push(section); // push the first one
+        else {
+          const prevType = arr[arr.length - 1].type;
+          const currType = section.type;
+          // if the current and previous types are groupable and the same type, group them into an array
+          if (groupableSections.includes(prevType) && groupableSections.includes(currType) && prevType === currType) {
+            arr.push(section);
+          }
+          // otherwise, push the section as-is
+          else {
+            innerGroupedSections.push(arr);
+            arr = [section];
+          }
+        }
+        return arr;
+      }, []));
+
+      groupedSections = innerGroupedSections.reduce((arr, group) => {
+        if (arr.length === 0 || group[0].type === "Grouping") arr.push([group]);
+        else arr[arr.length - 1].push(group);
+        return arr;
+      }, []);
+
+    }
+
     return (
       <div id="Profile">
 
@@ -291,6 +224,8 @@ class Profile extends Component {
         </Helmet>
 
         <Splash data={profile} comparisons={comparisons} />
+
+        <Subnav sections={groupedSections} />
 
         <Section
           data={{
@@ -317,30 +252,12 @@ class Profile extends Component {
           </Section>;
         }) }
 
-        <SubNav type="scroll" anchor="top" visible={() => {
-          if (typeof window === undefined) return false;
-          const elem = select("#Splash .profile-sections").node();
-          const top = elem.getBoundingClientRect().top;
-          return top && top <= 45;
-        }}>
-          <SectionIcon slug="about" title="About" active={ activeSection === "about" } />
-          { GroupingSections.map((s, i) => <SectionIcon key={i} {...s} active={ activeSection === s.slug } />) }
-        </SubNav>
-
         { similar.length && <div id="keep-exploring" className="keep-exploring">
           <h2>Keep Exploring</h2>
           <div className={`tiles ${slug}`}>
             { similar.map(d => <Tile key={d.slug || d.id} title={d.display || d.name} subtitle={d.hierarchy} image={`/api/profile/${slug}/${d.id}/thumb`} url={`/profile/${slug}/${d.slug || d.id}`} />) }
           </div>
         </div> }
-
-        <div className={`sidenav ${showSidenav ? "visible" : ""}`}>
-          { sidenav.map((s, i) => <div key={i} className="sidenav-section">
-            {s.map(t => <Tooltip position={Position.LEFT} className={`sidenav-circle ${t.slug === activeSidenav ? "active" : ""}`} key={t.slug} content={<span className="sidenav-label">{t.title}</span>}>
-              <AnchorLink to={t.slug}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</AnchorLink>
-            </Tooltip>)}
-          </div>) }
-        </div>
 
         { loading ? <Loading /> : null }
 
