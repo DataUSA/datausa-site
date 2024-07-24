8e0be13a004a513af2b278be3d9127ee07e41a34
nspmx <nico.perezmundaca@gmail.com>
2024-07-24 11:28:49 -0400

    add helm nginx upgrade script on the github workflow

---
+++ helm/nginx/helm-installation.sh
@@ -0,0 +1,14 @@
+helm upgrade --install --create-namespace \
+  nginx-ingress nginx-stable/nginx-ingress \
+  --set controller.name=nginx-ingress-canon \
+  --set controller.service.name=nginx-ingress-canon-service \
+  --set controller.enableSnippets=true \
+  --set controller.ingressClass.name=nginx-canon \
+  --set controller.ingressClass.create=true \
+  --set controller.image.tag=3.6.1 \
+  --set controller.volumes[0].name=canon-site-boston-cache \
+  --set controller.volumes[0].persistentVolumeClaim.claimName=canon-site-boston-pvc \
+  --set controller.volumeMounts[0].name=canon-site-boston-cache \
+  --set controller.volumeMounts[0].mountPath=/opt/sfw \
+  --set controller.volumeMounts[0].subPath=boston \
+  --namespace nginx-ingress-canon
