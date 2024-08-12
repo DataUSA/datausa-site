8c9c2bf0efe9e025039503e166d9d488f0aa5ebd
nspmx <nico.perezmundaca@gmail.com>
2024-08-12 15:44:52 -0400

    add persistentvolume own helm

--- helm/nginx/helm-installation.sh
+++ helm/nginx/helm-installation.sh
@@ -4,6 +4,13 @@ helm repo add nginx-stable https://helm.nginx.com/stable
 
 helm repo update
 
+helm upgrade --install --create-namespace \
+  --set controller.name=nginx-ingress-canon-cache \
+  --set persistentVolume.nfs.server=$CACHE_IP \
+  --values=./helm/nginx/canon.yaml \
+  ./helm/nginx \
+  --namespace nginx-ingress-canon
+
 # --set controller.service.loadBalancerIP="34.27.24.41" \
 helm upgrade --install --create-namespace \
   nginx-ingress-canon nginx-stable/nginx-ingress \
