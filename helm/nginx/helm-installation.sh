071954e2b31c8055e20b9a11a6c4b38e34b0177c
nspmx <nico.perezmundaca@gmail.com>
2024-08-13 15:47:18 -0400

    change boston values to cambridge to test

--- helm/nginx/helm-installation.sh
+++ helm/nginx/helm-installation.sh
@@ -5,10 +5,10 @@ helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 
 helm upgrade --install --create-namespace \
-  canon-site-boston-cache ./helm/nginx/persistentvolume \
-  --set fullnameOverride=canon-site-boston-cache \
+  canon-site-cambridge-cache ./helm/nginx/persistentvolume \
+  --set fullnameOverride=canon-site-cambridge-cache \
   --set persistentVolume.nfs.server=$CACHE_IP \
-  --values=./helm/boston.yaml \
+  --values=./helm/cambridge.yaml \
   --namespace nginx-ingress-canon
 
 # --set controller.service.loadBalancerIP="34.27.24.41" \
