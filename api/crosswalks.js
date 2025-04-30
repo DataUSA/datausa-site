3fc3921a83df554b3b7b35282bda529b423fed98
Alexandra <alexandra@datawheel.us>
2025-04-30 14:23:31 -0400

    modifies commonJobLookup to point to Tesseract

--- api/crosswalks.js
+++ api/crosswalks.js
@@ -10,7 +10,7 @@ const naics2io = loadJSON("/static/data/pums_naics_to_iocode.json");
 const naics2bls = loadJSON("/static/data/pums_bls_industry_crosswalk.json");
 const soc2bls = loadJSON("/static/data/pums_bls_occupation_crosswalk.json");
 
-const {CANON_API, CANON_GEOSERVICE_API} = process.env;
+const {CANON_API, CANON_GEOSERVICE_API, CANON_CONST_TESSERACT} = process.env;
 const geoOrder = ["Nation", "State", "County", "MSA", "Place", "PUMA"];
 
 module.exports = function(app) {
@@ -360,15 +360,27 @@ module.exports = function(app) {
    * 5 most specialized majors at University," requires that we construct an API request
    * WITH THOSE 5 MAJORS in the url. This crosswalk is responsible for constructing that request.
    */
-  app.get("/api/university/commonJobLookup/:id", async(req, res) => {
+  app.get("/api/university/commonJobLookup/:id/:hierarchy", async(req, res) => {
+
+    const ipedsPumsFilterTesseract = d => ![21, 29, 32, 33, 34, 35, 36, 37, 48, 53, 60].includes(d["CIP2 ID"]);
 
     const {id} = req.params;
+    const {hierarchy} = req.params;
 
-    const cipURL = `${CANON_API}/api/data?University=${id}&measures=Completions,yuc%20RCA&year=latest&drilldowns=CIP2&order=yuc%20RCA&sort=desc`;
+    const latestYearUrl = `${CANON_CONST_TESSERACT}tesseract/data.jsonrecords?cube=ipeds_completions&drilldowns=Year&include=${hierarchy}:${id}&locale=en&measures=Completions&time=Year.latest`
+    const latestYear = await axios.get(latestYearUrl)
+      .then(resp => resp.data.data[0]["Year"]);
+
+    const cipURL = `${CANON_CONST_TESSERACT}complexity/rca_historical.jsonrecords?cube=ipeds_completions&location=${hierarchy}&activity=CIP2&measure=Completions&time=Year&filter=${hierarchy}:${id}&cuts=Year:${latestYear}`
+
     const CIP2 = await axios.get(cipURL)
-      .then(resp => resp.data.data.filter(ipedsPumsFilter).slice(0, 5).map(d => d["ID CIP2"]).join());
+      .then(resp => resp.data.data
+        .sort((a, b) => b["Completions RCA"] - a["Completions RCA"])
+        .filter(ipedsPumsFilterTesseract)
+        .slice(0, 5)
+        .map(d => `${d["CIP2 ID"]}`.padStart(2, '0')).join());
 
-    const logicUrl = `${CANON_API}/api/data?measures=Total%20Population,Record%20Count&year=latest&drilldowns=CIP2,Detailed%20Occupation&order=Total%20Population&sort=desc&Workforce%20Status=true&Employment%20Time%20Status=1&Record%20Count%3E=5&CIP2=${CIP2}`;
+    const logicUrl = `${CANON_CONST_TESSERACT}tesseract/data.jsonrecords?cube=pums_5&drilldowns=Year,CIP2,Detailed+Occupation&include=Workforce+Status:true;Employment+Time+Status:1;CIP2:${CIP2}&locale=en&measures=Record+Count,Total+Population&time=Year.latest&filters=Record+Count.gte.5&sort=Total+Population.desc`
     const jobList = await axios.get(logicUrl)
       .then(resp => resp.data.data)
       .catch(() => []);
