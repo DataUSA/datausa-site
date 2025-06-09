a2108bac17e6b40b27170c6dc89241227bde5edf
Alexandra <alexandra@datawheel.us>
2025-06-09 17:31:24 -0400

    migrate /cache/urls to point to tesseract

--- cache/urls.js
+++ cache/urls.js
@@ -1,15 +1,18 @@
 const axios = require("axios");
-const {CANON_LOGICLAYER_CUBE} = process.env;
-const prefix = `${CANON_LOGICLAYER_CUBE}${CANON_LOGICLAYER_CUBE.slice(-1) === "/" ? "" : "/"}`;
+const {CANON_CONST_TESSERACT} = process.env;
+const prefix = `${CANON_CONST_TESSERACT}${CANON_CONST_TESSERACT.slice(-1) === "/" ? "" : "/"}`;
 
-module.exports = async function() {
+module.exports = async function () {
 
-  return axios.get(`${prefix}cubes/ipeds_completions/dimensions/University/hierarchies/University/levels/University/members?member_properties[]=URL`)
+  return axios.get(`${prefix}tesseract/data.jsonrecords?cube=university_cube&drilldowns=University,URL&locale=en&measures=Count`)
     .then(resp => resp.data)
-    .then(data => data.members.reduce((acc, d) => {
-      acc[d.key] = d.properties.URL;
-      return acc;
-    }, {}))
+    .then(data => {
+      const result = data.data.reduce((acc, d) => {
+        acc[d["University ID"]] = d.URL;
+        return acc;
+      }, {});
+      return result;
+    })
     .catch(err => {
       console.error(` 🌎  URL Cache Error: ${err.message}`);
       if (err.config) console.error(err.config.url);
