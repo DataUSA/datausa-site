929eed921f52d4ef09a75573b21039f808d02dd1
Alexandra <alexandra@datawheel.us>
2025-06-10 12:47:10 -0400

    migrate /cache/pops to tesseract

--- cache/pops.js
+++ cache/pops.js
@@ -1,30 +1,22 @@
-const {Client, MondrianDataSource} = require("@datawheel/olap-client");
-const {CANON_LOGICLAYER_CUBE} = process.env;
+const axios = require("axios");
+const {CANON_CONST_TESSERACT} = process.env;
+const prefix = `${CANON_CONST_TESSERACT}${CANON_CONST_TESSERACT.slice(-1) === "/" ? "" : "/"}`;
 
 module.exports = async function() {
 
-  const datasource = new MondrianDataSource(CANON_LOGICLAYER_CUBE);
-  const client = new Client(datasource);
-
   const levels = ["Nation", "State", "County", "MSA", "Place", "PUMA", "Congressional District"];
-  const popQueries = levels
-    .map(level => client.getCube("acs_yg_total_population_5")
-      .then(c => {
-        const query = c.query
-          .addDrilldown(level)
-          .addMeasure("Population")
-          .addCut("[Year].[Year]", ["2019"])
-          .setFormat("jsonrecords");
-        return client.execQuery(query);
-      })
-      .then(resp => resp.data.reduce((acc, d) => {
-        acc[d[`ID ${level}`]] = d.Population;
+  const popQueries = levels.map(level => {
+    const url = `${prefix}tesseract/data.jsonrecords?cube=acs_yg_total_population_5&drilldowns=${encodeURIComponent(level)}&locale=en&measures=Population&time=Year.latest`;
+    return axios.get(url)
+      .then(resp => resp.data.data.reduce((acc, d) => {
+        acc[d[`${level} ID`]] = d.Population;
         return acc;
       }, {}))
       .catch(err => {
         console.error(` 🌎  ${level} Pop Cache Error: ${err.message}`);
         if (err.config) console.error(err.config.url);
-      }));
+      });
+  });
 
   const rawPops = await Promise.all(popQueries);
   const pops = rawPops.reduce((obj, d) => (obj = Object.assign(obj, d), obj), {});
