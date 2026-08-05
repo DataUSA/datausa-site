1d77369625e8ba4627179ecdf9339708ea635410
D2SD <samueloso21@gmail.com>
2026-08-05 14:20:30 -0400

    add service account to pull image

--- deploy_to_vm.sh
+++ deploy_to_vm.sh
@@ -2,6 +2,9 @@
 # Pull the latest version and create the containers if they don't exist
 export $(cat .env.gcp | xargs)
 
+echo "Docker Login"
+cat ./serviceAccountKey.json | docker login -u _json_key --password-stdin https://${GCP_ARTIFACT_REGISTRY_LOCATION}-docker.pkg.dev
+
 echo "Pulling the latest version and creating the containers if they don't exist"
 docker compose --env-file .env.gcp pull
 
